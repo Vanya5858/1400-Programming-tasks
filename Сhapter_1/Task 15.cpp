9fd9fd1b8abad202485cdc34824f6d208c1e683f
Ivan <iv.vano77@mail.ru>
2025-10-24 13:39:02 +0300

    Add files via upload

---
+++ Сhapter_1/Task 15.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d;
+    setlocale(LC_ALL, "RU");
+    cout << "Введите 4 различных числа через пробел: ";
+    cin >> a >> b >> c >> d;
+    cout << "\nВы ввели: " << a << " " << b << " " << c << " " << d;
+}
