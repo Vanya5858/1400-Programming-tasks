28f08be07bc7c867cd4ceacde871336350471b4c
Ivan <iv.vano77@mail.ru>
2025-10-24 14:26:36 +0300

    Add files via upload

---
+++ Сhapter_2/Task_2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int a;
+    double res;
+    cout << "Программа для вычисления функции\n\n" << "a) y = a^2 + 10/(sqrt(a^2 + 1))\n" << "Введите значени a: ";
+    cin >> a;
+    if (sqrt(a * a + 1) < 0) {
+        cout << "Ошибка. Корень и знаменатель не могут быть отрицательными числами.";
+    }
+    else {
+        res = (a * a + 10) / (sqrt(a * a + 1));
+        cout << "Ответ: " << res;
+    }
+    return 0;
+}
