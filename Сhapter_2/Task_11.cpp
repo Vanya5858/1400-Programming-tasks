bf9b1b70131ac4b5e273bc5ad54b24754c1984a0
Ivan <iv.vano77@mail.ru>
2025-10-25 23:37:53 +0300

    Add files via upload

---
+++ Сhapter_2/Task_11.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RU");
+	double v, m, p;
+	cout << "\t\tОпределение плотности(P) материала тела\n";
+	cout << "Введите Объём тела(V): ";
+	cin >> v;
+	cout << "Введите массу тела(m): ";
+	cin >> m;
+	p = m / v;
+	cout << "\nОтвет: P = " << p << "\n\n";
+	return 0;
+}
