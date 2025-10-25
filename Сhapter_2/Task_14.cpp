cda031d3a0d2dc9f672a3836d475822dbc7630da
Ivan <iv.vano77@mail.ru>
2025-10-26 00:45:32 +0300

    Add files via upload

---
+++ Сhapter_2/Task_14.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    double a, b, c;
+    cout << "\t\t\tНахождение Гипотенузы через катеты треугольника\n";
+    cout << "Введите 1 катет(a): ";
+    cin >> a;
+    cout << "Введите 2 катет(b): ";
+    cin >> b;
+
+    // Проверка катетов на отрицание
+    if ((a < 0) or (b < 0)) {
+        cout << "\nВы ввели отрицательное значение.\n";
+        return 1;
+    }
+    else {
+        c = sqrt(pow(a, 2) + pow(b, 2));
+        cout << "\nОтвет: " << c << "\n";
+    }
+}
---
+++ Сhapter_2/Task_16.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    double a, b, c, P;
+    cout << "\t\t\tНахождение Периметра прямоугольного треугольника через катеты\n";
+    cout << "Введите 1 катет(a): ";
+    cin >> a;
+    cout << "Введите 2 катет(b): ";
+    cin >> b;
+
+    // Проверка катетов на отрицание
+    if ((a < 0) or (b < 0)) {
+        cout << "\nВы ввели отрицательное значение.\n";
+        return 1;
+    }
+    else {
+        c = sqrt(pow(a, 2) + pow(b, 2));
+    }
+    P = a + b + c;
+    cout << "\nОтвет: Периметр P = " << P << "\n";
+    return 0;
+}
