bc39881f4aeb863e54da00435240b9ce980053ec
Ivan <iv.vano77@mail.ru>
2025-10-25 23:51:27 +0300

    Add files via upload

---
+++ Сhapter_2/Task_12.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    int D, N, S;
+    cout << "\t\t\tПрограмма для вычисления плотности населения(D)\n";
+    cout << "Введите количество жителей района(N чел): ";
+    cin >> N;
+    cout << "Введите площать данного района(S Км^2): ";
+    cin >> S;
+    D = N / S;
+    cout << "\nОтвет: D = N / S = " << D << " чел./км^2\n\n";
+    return 0;
+}
