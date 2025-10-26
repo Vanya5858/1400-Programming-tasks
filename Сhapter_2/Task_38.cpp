ba85c5ab5292cb1b8d98876bf662ee27e31395e0
Ivan <iv.vano77@mail.ru>
2025-10-27 02:04:29 +0300

    Add files via upload

---
+++ Сhapter_2/Task_38.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b;
+
+    // Ввод двух целых чисел
+    cout << "Введите два целых числа через пробел: ";
+    cin >> a >> b;
+
+    // Вычисление операций
+    int sum = a + b;
+    int difference = a - b;
+    int product = a * b;
+
+    // Вывод результатов
+    cout << a << "+" << b << "=" << sum << "\n";
+    cout << a << "-" << b << "=" << difference << "\n";
+    cout << a << "*" << b << "=" << product << "\n";
+
+    // Деление с проверкой на ноль
+    if (b != 0) {
+        double quotient = (double)a / b;  // обычное приведение типа
+        cout << a << "/" << b << "=" << quotient << "\n";
+    }
+    else {
+        cout << a << "/" << b << "= невозможно (деление на ноль)" << "\n";
+    }
+
+    // Среднее арифметическое
+    double average = (a + b) / 2.0;  // 2.0 - вещественное число
+    cout << "(" << a << "+" << b << ")/2=" << average << "\n";
+
+    return 0;
+}
