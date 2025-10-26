f4001007cdb78b2562a644d66fa7e8358a5d203d
Ivan <iv.vano77@mail.ru>
2025-10-26 23:18:21 +0300

    Add files via upload

---
+++ Сhapter_2/Task_28.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double PI = 3.1415926;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, angle_degrees;
+
+    // Ввод данных
+    cout << "\t\t\tВычисление площади равнобедренной трапеции\n";
+    cout << "Введите длину большего основания a: ";
+    cin >> a;
+    cout << "Введите длину меньшего основания b: ";
+    cin >> b;
+    cout << "Введите угол при большем основании (в градусах): ";
+    cin >> angle_degrees;
+
+    // Проверка корректности данных
+    if (a <= 0 || b <= 0 || angle_degrees <= 0 || angle_degrees >= 90) {
+        cout << "Ошибка: некорректные данные!\n";
+        cout << "- Основания должны быть положительными\n";
+        cout << "- Большее основание должно быть больше меньшего\n";
+        cout << "- Угол должен быть между 0 и 90\n";
+        return 1;
+    }
+
+    if (b >= a) {
+        cout << "Ошибка: основание a должно быть больше основания b!\n";
+        return 1;
+    }
+
+    // Перевод угла из градусов в радианы
+    double angle_radians = angle_degrees * PI / 180.0;
+
+    // Вычисление проекции боковой стороны
+    double projection = (a - b) / 2;
+
+    // Вычисление высоты трапеции
+    double h = projection * tan(angle_radians);
+
+    // Вычисление площади
+    double S = (a + b) * h / 2;
+
+    // Вычисление боковой стороны (дополнительно)
+    double side = projection / cos(angle_radians);
+
+    // Вывод результата
+    cout << "\nРезультат:\n";
+    cout << "Большее основание a = " << a << "\n";
+    cout << "Меньшее основание b = " << b << "\n";
+    cout << "Угол при основании A = " << angle_degrees << "\n";
+    cout << "Площадь S = (a + b) * h / 2 = " << S << "\n";
+
+    return 0;
+}
---
+++ Сhapter_2/Task_29.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+	double x_1, x_2, y_1, y_2, x_3, y_3, distance_1, distance_2, distance_3, P, S, Semi_P;
+
+	// Ввод координат 3 точек
+	cout << "\t\tНахождение площади и периметра треугольника, изображенного на координатной плоскости\n";
+	cout << "Введите координаты 1 точки: \n";
+	cout << "x = ";
+	cin >> x_1;
+	cout << "y = ";
+	cin >> y_1;
+	cout << "Введите координаты 2 точки: \n";
+	cout << "x = ";
+	cin >> x_2;
+	cout << "y = ";
+	cin >> y_2;
+	cout << "Введите координаты 3 точки: \n";
+	cout << "x = ";
+	cin >> x_3;
+	cout << "y = ";
+	cin >> y_3;
+
+	// Вычисления Расстояния между 2 точками(3 разных вершин)
+	distance_1 = sqrt(pow(fabs(x_1 - x_2), 2) + pow(fabs(y_1 - y_2), 2));
+	distance_2 = sqrt(pow(fabs(x_1 - x_3), 2) + pow(fabs(y_1 - y_3), 2));
+	distance_3 = sqrt(pow(fabs(x_2 - x_3), 2) + pow(fabs(y_2 - y_3), 2));
+
+	// Проверка на существование треугольника
+	if (distance_1 + distance_2 <= distance_3 || distance_1 + distance_3 <= distance_2 || distance_2 + distance_3 <= distance_1) {
+		cout << "Ошибка: точки лежат на одной прямой!" << endl;
+		return 1;
+	}
+
+	// Нахождение периметра треугольника
+	P = distance_1 + distance_2 + distance_3;
+	Semi_P = P / 2;
+
+	// Нахождения площади треугольника(Через полупериметр)
+	S = sqrt(Semi_P * (Semi_P - distance_1) * (Semi_P - distance_2) * (Semi_P - distance_3));
+
+	// Вывод результата
+	cout << "\nРезультат:" << "\n";
+	cout << "Точка A: (" << x_1 << ", " << y_1 << ")" << "\n";
+	cout << "Точка B: (" << x_2 << ", " << y_2 << ")" << "\n";
+	cout << "Точка C: (" << x_3 << ", " << y_3 << ")" << "\n";
+	cout << "Ответ: P = " << P << "; S = " << S << "\n";
+
+	return 0;
+}
