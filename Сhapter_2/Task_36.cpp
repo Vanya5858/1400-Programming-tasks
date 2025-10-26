97d0571e1b0b02d232c3667d37b93e1db163d908
Ivan <iv.vano77@mail.ru>
2025-10-27 01:56:29 +0300

    Add files via upload

---
+++ Сhapter_2/Task_36.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double celsius;
+
+    cout << "\t\t\tПеревод из градусов цельсия в Фаренгейты и Кельвины\n";
+
+    // Ввод температуры в градусах Цельсия
+    cout << "Введите температуру в градусах Цельсия: ";
+    cin >> celsius;
+
+    // Перевод в Фаренгейты
+    double fahrenheit = (celsius * 1.8) + 32;
+
+    // Перевод в Кельвины
+    double kelvin = celsius + 273.15;
+
+    // Вывод результатов
+    cout << "\nТемпература в различных шкалах:\n";
+    cout << "Цельсий: " << celsius << " C\n";
+    cout << "Фаренгейт: " << fahrenheit << " F\n";
+    cout << "Кельвин: " << kelvin << " K\n";
+
+    return 0;
+}
