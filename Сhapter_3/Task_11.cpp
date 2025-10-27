856ee1be45948a15d889d355ff9b7397cd02e1e9
Ivan <iv.vano77@mail.ru>
2025-10-28 00:34:00 +0300

    Add files via upload

---
+++ Сhapter_3/Task_11.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n, x;
+
+    // Ввод количества месяцев
+    cout << "Введите количество месяцев n: ";
+    cin >> n;
+
+    // Проверка корректности данных
+    if (n < 0) {
+        cout << "Ошибка: количество месяцев не может быть отрицательным!\n\n";
+        return 1;
+    }
+
+    // Вычисление номера месяца
+    x = (n + 1) % 12;
+    if (x == 0) x = 12;  // если остаток 0, то это декабрь (12 месяц)
+
+    // Вывод результата
+    cout << "С начала 1990 года по некоторый день прошло " << n << " месяцев и 2 дня\n";
+    cout << "Текущий месяц: ";
+    switch (x) {
+    case 1: cout << "январь"; break;
+    case 2: cout << "февраль"; break;
+    case 3: cout << "март"; break;
+    case 4: cout << "апрель"; break;
+    case 5: cout << "май"; break;
+    case 6: cout << "июнь"; break;
+    case 7: cout << "июль"; break;
+    case 8: cout << "август"; break;
+    case 9: cout << "сентябрь"; break;
+    case 10: cout << "октябрь"; break;
+    case 11: cout << "ноябрь"; break;
+    case 12: cout << "декабрь"; break;
+    }
+    cout << " (x=" << x << ")" << endl;
+
+    return 0;
+}
---
+++ Сhapter_3/Task_12.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int apartment_number;
+
+    // Ввод номера квартиры
+    cout << "Введите номер квартиры: ";
+    cin >> apartment_number;
+
+    // Вычисление номера этажа (на каждом этаже 4 квартиры)
+    int floor_number = (apartment_number - 1) / 4 + 1;
+
+    // Вычисление порядкового номера квартиры на этаже
+    int apartment_on_floor = (apartment_number - 1) % 4 + 1;
+
+    // Вывод результатов
+    cout << "Квартира #" << apartment_number << " находится:\n";
+    cout << "1) На " << floor_number << " этаже\n";
+    cout << "2) Является " << apartment_on_floor << "-й по счету на этаже\n";
+
+    return 0;
+}
