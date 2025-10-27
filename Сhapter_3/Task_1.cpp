d3e94ab1cf221defc8770fbcd683368df97ddf25
Ivan <iv.vano77@mail.ru>
2025-10-27 22:10:57 +0300

    Add files via upload

---
+++ Сhapter_3/Task_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "RU");
+    int centimeters, meters;
+
+    cout << "\t\t\tПеревод сантиметров в метры\n";
+    cout << "Введите длину в сантиметрах: ";
+    cin >> centimeters;
+    
+    // Проверка на отрицательное значение
+    if (centimeters < 0) {
+        cout << "Ошибка: расстояние не может быть отрицательным!\n";
+        return 1;
+    }
+
+    //Перевод сантиметров в метры
+    meters = centimeters / 100;
+
+    cout << "Расстояние: " << centimeters << " см\n";
+    cout << "Полных метров: " << meters << " м\n";
+    return 0;
+}
