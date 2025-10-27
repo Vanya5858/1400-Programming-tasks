910aab50f284f6f979d0a8ece971c3a3c585a42d
Ivan <iv.vano77@mail.ru>
2025-10-28 00:31:46 +0300

    Add files via upload

---
+++ Сhapter_3/Task_7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int apartment_number;
+
+    // Ввод номера квартиры
+    cout << "Введите номер квартиры: ";
+    cin >> apartment_number;
+
+    // Проверка корректности данных
+    if (apartment_number < 1 || apartment_number > 15) {
+        cout << "Ошибка: в подъезде только 15 квартир (номера с 1 по 15)!\n\n";
+        return 1;
+    }
+
+    int floor_number = (apartment_number - 1) / 3 + 1;
+
+    // Вывод результата
+    cout << "Квартира #" << apartment_number << " находится на " << floor_number << " этаже\n\n";
+
+    return 0;
+}
