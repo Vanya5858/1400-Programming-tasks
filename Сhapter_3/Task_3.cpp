6d9faa03ad1c410f94fa23f2736c67a3bf38f7e2
Ivan <iv.vano77@mail.ru>
2025-10-28 00:30:11 +0300

    Add files via upload

---
+++ Сhapter_3/Task_3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int days = 234;  // заданное количество дней
+
+    // Вычисление числа полных недель (1 неделя = 7 дней)
+    int weeks = days / 7;
+
+    // Вычисление оставшихся дней
+    int remaining_days = days % 7;
+
+    // Вывод результатов
+    cout << "Прошло дней: " << days << endl;
+    cout << "Полных недель: " << weeks << endl;
+    cout << "Остаток дней: " << remaining_days << endl;
+
+    return 0;
+}
