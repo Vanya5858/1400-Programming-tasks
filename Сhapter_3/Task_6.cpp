00f0b7ac8ff3271652813bb25f2644e17adbb2c0
Ivan <iv.vano77@mail.ru>
2025-10-28 00:31:10 +0300

    Add files via upload

---
+++ Сhapter_3/Task_6.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int seat_number;
+
+    // Ввод номера места
+    cout << "Введите номер места: ";
+    cin >> seat_number;
+
+    // Проверка корректности данных
+    if (seat_number < 1) {
+        cout << "Ошибка: Нет отрицательных мест!\n\n";
+        return 1;
+    }
+
+    int compartment_number = ((seat_number -1) / 4) + 1;
+
+    // Вывод результата
+    cout << "Место #" << seat_number << " находится в купе #" << compartment_number << endl;
+
+    return 0;
+}
