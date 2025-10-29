cbb2f40a9543677163e402df935ca800a976d915
Ivan <iv.vano77@mail.ru>
2025-10-29 11:49:21 +0300

    Add files via upload

---
+++ Сhapter_3/Task_22.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int number;
+
+    cout << "Введите трехзначное число: ";
+    cin >> number;
+
+    if (number >= 100 && number <= 999) {
+        int newNumber = (number % 100) * 10 + (number / 100);
+        cout << "Исходное число: " << number << endl;
+        cout << "Полученное число: " << newNumber << endl;
+    }
+    else {
+        cout << "Ошибка! Введите трехзначное число (100-999)\n";
+    }
+
+    return 0;
+}
