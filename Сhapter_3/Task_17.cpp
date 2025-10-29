ba878dd53fe36ad6ed06bd05730e4a9e87f53fb7
Ivan <iv.vano77@mail.ru>
2025-10-29 11:48:10 +0300

    Add files via upload

---
+++ Сhapter_3/Task_17.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int number;
+
+    cout << "Введите двузначное число: ";
+    cin >> number;
+
+    // Проверка и расчет в одном блоке
+    if (number >= 10 && number <= 99) {
+        int sum = (number / 10) + (number % 10);
+        cout << "Сумма цифр числа " << number << " равна: " << sum << endl;
+    }
+    else {
+        cout << "Ошибка! Введите двузначное число (10-99)\n";
+    }
+
+    return 0;
+}
