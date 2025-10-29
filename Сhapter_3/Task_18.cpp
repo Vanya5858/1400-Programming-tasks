55a4e296976546fa171fbe3b5dc2612270a25bfc
Ivan <iv.vano77@mail.ru>
2025-10-29 11:48:23 +0300

    Add files via upload

---
+++ Сhapter_3/Task_18.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int number;
+
+    cout << "Введите двузначное число: ";
+    cin >> number;
+
+    if (number >= 10 && number <= 99) {
+        int reversedNumber = (number % 10) * 10 + (number / 10);
+        cout << "Исходное число: " << number << endl;
+        cout << "Число после перестановки: " << reversedNumber << endl;
+    }
+    else {
+        cout << "Ошибка! Введите двузначное число (10-99)\n";
+    }
+
+    return 0;
+}
