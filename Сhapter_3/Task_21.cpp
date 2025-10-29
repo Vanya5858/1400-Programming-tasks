3b7c5ea5bad1a041fda23d6029b5b5e3bd6a7bfb
Ivan <iv.vano77@mail.ru>
2025-10-29 11:49:08 +0300

    Add files via upload

---
+++ Сhapter_3/Task_21.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int number;
+
+    cout << "Введите трехзначное число: ";
+    cin >> number;
+
+    if (number >= 100 && number <= 999) {
+        int reversed = (number % 10) * 100 + ((number / 10) % 10) * 10 + (number / 100);
+        cout << "Исходное число: " << number << endl;
+        cout << "Число справа налево: " << reversed << endl;
+    }
+    else {
+        cout << "Ошибка! Введите трехзначное число (100-999)\n";
+    }
+
+    return 0;
+}
