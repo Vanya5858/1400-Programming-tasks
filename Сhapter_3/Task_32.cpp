9c917df4e9f7de7a2f222ef8e9016b1dbc9e0f6b
Ivan <iv.vano77@mail.ru>
2025-10-30 01:28:14 +0300

    Add files via upload

---
+++ Сhapter_3/Task_32.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL, "RU");
+    
+    int result = 237;
+    int a, bc, x;
+
+    a = result / 100;
+    bc = result % 100;
+
+    x = (bc * 10) + a;
+    cout << "Изначальное число x: " << x << endl;
+    
+    // Проверка
+    cout << "Проверка:\n";
+    cout << "1. Вычитаем последнюю цифру: " << x << " - " << a << " = " << (x - a) << endl;
+    cout << "2. Делим на 10: " << (x - a) << " / 10 = " << (x - a) / 10 << endl;
+    cout << "3. Приписываем слева " << a << ": получаем " << result << endl << endl;
+    return 0;
+}
