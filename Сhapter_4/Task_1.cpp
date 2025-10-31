b40ca1d57aa7d9e40f81339950f6dd295521f26a
Ivan <iv.vano77@mail.ru>
2025-10-31 09:18:18 +0300

    Add files via upload

---
+++ Сhapter_4/Task_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b;
+
+    cout << "Введите два различных вещественных числа: ";
+    cin >> a >> b;
+
+    // Проверяем, что числа различные
+    if (a == b) {
+        cout << "Ошибка! Числа должны быть различными.\n";
+        return 1;
+    }
+
+    // а) Какое число больше
+    double max_num = a * (a > b) + b * (b > a);
+
+    // б) Какое число меньше  
+    double min_num = a * (a < b) + b * (b < a);
+
+    cout << "а) Большее число: " << max_num << endl;
+    cout << "б) Меньшее число: " << min_num << endl;
+
+    return 0;
+}
