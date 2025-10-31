7c76dcd5b35e3824a407966dadcd579d1199cdaf
Ivan <iv.vano77@mail.ru>
2025-10-31 09:18:44 +0300

    Add files via upload

---
+++ Сhapter_4/Task_9.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b;
+
+    cout << "Введите два различных вещественных числа: ";
+    cin >> a >> b;
+
+    // Проверяем, что числа различные
+    if (a == b) {
+        cout << "Ошибка! Числа должны быть различными." << endl;
+        return 1;
+    }
+
+    double max_num, min_num;
+
+    // ЕДИНСТВЕННЫЙ условный оператор
+    if (a > b) {
+        max_num = a;
+        min_num = b;
+    }
+    else {
+        max_num = b;
+        min_num = a;
+    }
+
+    cout << "Максимальное значение: " << max_num << endl;
+    cout << "Минимальное значение: " << min_num << endl;
+
+    return 0;
+}
