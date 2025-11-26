6a4fe42815781eb59797c8d1f5c70a75ce025475
Ivan <iv.vano77@mail.ru>
2025-11-26 23:44:45 +0300

    Add files via upload

---
+++ Сhapter_5/Task_10.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double exchange_rate;
+
+    // Ввод курса доллара
+    cout << "Введите текущий курс доллара (руб. за 1 USD): ";
+    cin >> exchange_rate;
+
+    cout << "\nТаблица перевода USD в RUB:\n";
+    cout << "Курс: 1 USD = " << fixed << setprecision(2) << exchange_rate << " RUB\n\n";
+    cout << "USD    RUB\n";
+    cout << "-----------\n";
+
+    for (int dollars = 1; dollars <= 20; dollars++) {
+        double rubles = dollars * exchange_rate;
+        cout << dollars << "   " << fixed << setprecision(2) << setw(8) << rubles << endl;
+    }
+
+    return 0;
+}
