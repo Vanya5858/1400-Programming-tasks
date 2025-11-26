1346bfd636292493b24435db29885275b6fc61b5
Ivan <iv.vano77@mail.ru>
2025-11-27 00:44:00 +0300

    Add files via upload

---
+++ Сhapter_5/Task_40.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    long long number;
+    cout << "Введите 9-значное число: ";
+    cin >> number;
+
+    int sum = 0;
+
+    // Последовательно извлекаем цифры справа налево
+    while (number > 0) {
+        sum += number % 10;  // получаем последнюю цифру
+        number /= 10;        // удаляем последнюю цифру
+    }
+
+    cout << "Сумма цифр: " << sum << endl;
+
+    return 0;
+}
