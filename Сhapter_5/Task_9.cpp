0c24483382ea453a5980ee4dae29be20ef6582b0
Ivan <iv.vano77@mail.ru>
2025-11-26 23:44:33 +0300

    Add files via upload

---
+++ Сhapter_5/Task_9.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const double INCH_TO_MM = 25.4;
+    const double MM_TO_CM = 0.1;
+
+    cout << "Дюймы   Сантиметры\n";
+    cout << "------------------\n";
+
+    for (int inches = 10; inches <= 22; inches++) {
+        double centimeters = inches * INCH_TO_MM * MM_TO_CM;
+        cout << inches << "      " << fixed << setprecision(2) << setw(6) << centimeters << endl;
+    }
+
+    return 0;
+}
