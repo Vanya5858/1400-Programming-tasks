afb6161dfb62a5295cd7b6b8e34a51c78cf43641
Ivan <iv.vano77@mail.ru>
2025-11-26 23:44:20 +0300

    Add files via upload

---
+++ Сhapter_5/Task_8.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const double POUND_TO_KG = 0.453; // 1 фунт = 0.453 кг
+
+    cout << "Фунты   Кг\n";
+    cout << "------------\n";
+
+    for (int pounds = 1; pounds <= 10; pounds++) {
+        double kilograms = pounds * POUND_TO_KG;
+        cout << pounds << "    " << fixed << setprecision(3) << kilograms << endl;
+    }
+
+    return 0;
+}
