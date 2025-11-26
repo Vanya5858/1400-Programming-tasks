7081e22db1b53a4ea84fd460e13971541fcc08af
Ivan <iv.vano77@mail.ru>
2025-11-27 00:42:25 +0300

    Add files via upload

---
+++ Сhapter_5/Task_33.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double sum = 0;
+
+    for (int i = 2; i <= 10; i++) {
+        sum += (double)i / (i + 1);
+    }
+
+    cout << "2/3 + 3/4 + 4/5 + ... + 10/11 = " << sum << endl;
+
+    return 0;
+}
