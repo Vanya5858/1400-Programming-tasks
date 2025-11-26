acdb854782330afd11841c2baace21ec3dd59d9a
Ivan <iv.vano77@mail.ru>
2025-11-27 00:43:08 +0300

    Add files via upload

---
+++ Сhapter_5/Task_36.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    double sum = 0;
+    double term = 1.0;  // первый член последовательности: 1/3^0 = 1
+
+    for (int i = 0; i <= 8; i++) {
+        sum += term;
+        term /= 3;      // каждый следующий член в 3 раза меньше
+    }
+
+    cout << "1 + 1/3 + 1/3^2 + ... + 1/3^8 = " << sum << endl;
+
+    return 0;
+}
