d232f21f10b56c83ff991443fbe43863b5bde088
Ivan <iv.vano77@mail.ru>
2025-11-26 23:42:58 +0300

    Add files via upload

---
+++ Сhapter_5/Task_2.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int number, count;
+
+    cout << "Введите число: ";
+    cin >> number;
+    cout << "Введите количество повторений: ";
+    cin >> count;
+
+    for (int i = 0; i < count; i++) {
+        if (i > 0) {
+            cout << " ";
+        }
+        cout << number;
+    }
+    cout << endl;
+    return 0;
+}
