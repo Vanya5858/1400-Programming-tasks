aabb599e526b08b6bface62731f343414be595f3
Ivan <iv.vano77@mail.ru>
2025-11-26 23:44:08 +0300

    Add files via upload

---
+++ Сhapter_5/Task_7.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double price = 20.4;
+
+    cout << "Стоимость товара:\n";
+    for (int i = 2; i <= 20; i++) {
+        cout << i << " шт. - " << fixed << setprecision(2) << i * price << " руб.\n";
+    }
+
+    return 0;
+}
