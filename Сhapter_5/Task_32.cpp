07d36f63b63d857671bcc6850f99c4d82253dde4
Ivan <iv.vano77@mail.ru>
2025-11-27 00:42:03 +0300

    Add files via upload

---
+++ Сhapter_5/Task_32.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите натуральное число n: ";
+    cin >> n;
+
+    double sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += 1.0 / i;
+    }
+
+    cout << "1 + 1/2 + 1/3 + ... + 1/" << n << " = " << sum << endl;
+
+    return 0;
+}
