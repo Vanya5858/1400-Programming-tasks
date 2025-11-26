c8dfc75622efefe6e9e49cbf90bbb19f1ebfc0ea
Ivan <iv.vano77@mail.ru>
2025-11-26 23:45:41 +0300

    Add files via upload

---
+++ Сhapter_5/Task_14.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int multiplier = 9;
+
+    cout << "Таблица умножения на " << multiplier << ":\n";
+    cout << "-------------------\n";
+
+    for (int i = 1; i <= 9; i++) {
+        cout << i << " x " << multiplier << " = " << i * multiplier << endl;
+    }
+
+    return 0;
+}
