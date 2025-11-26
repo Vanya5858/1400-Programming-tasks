18e314a467c642fee9bbd9caa7fa6b9b28a923e6
Ivan <iv.vano77@mail.ru>
2025-11-27 00:42:54 +0300

    Add files via upload

---
+++ Сhapter_5/Task_35.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите натуральное число n (n >= 2): ";
+    cin >> n;
+
+    long long sum = 0;
+    for (int i = 1; i <= n - 1; i++) {
+        sum += (long long)i * (i + 1);
+    }
+
+    cout << "1*2 + 2*3 + ... + (" << n - 1 << ")*" << n << " = " << sum << endl;
+
+    return 0;
+}
