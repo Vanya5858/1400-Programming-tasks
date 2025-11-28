e1d22c5d3fa9c634ce16c6fc37a066a1723f8341
Ivan <iv.vano77@mail.ru>
2025-11-29 02:40:17 +0300

    Add files via upload

---
+++ Сhapter_4/Task_4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double x, y;
+    cout << "Введите координаты точки (x y): ";
+    cin >> x >> y;
+
+    if (x < 4) {
+        cout << "Точка находится в области I\n";
+    }
+    else {
+        cout << "Точка находится в области II\n";
+    }
+    return 0;
+}
