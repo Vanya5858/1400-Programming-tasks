382349f0e0e7ee6cfb83e259b6d4cc694096ec34
Ivan <iv.vano77@mail.ru>
2025-11-28 10:40:33 +0300

    Add files via upload

---
+++ Сhapter_5/Task_77.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    cout << "Нечетные двузначные числа, у которых последняя цифра равна 3 или 7:\n";
+
+    int count = 0;
+    for (int i = 10; i <= 99; i++) {
+        if (i % 2 != 0) { // нечетное
+            int last_digit = i % 10;
+            if (last_digit == 3 or last_digit == 7) {
+                cout << i << " ";
+                count++;
+                if (count % 10 == 0) cout << endl;
+            }
+        }
+    }
+
+    cout << "\nВсего чисел: " << count << endl;
+
+    return 0;
+}
