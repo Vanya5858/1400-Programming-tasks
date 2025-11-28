678c9ad00cf08801ea98b5562abd4efc77d79fa9
Ivan <iv.vano77@mail.ru>
2025-11-28 10:40:07 +0300

    Add files via upload

---
+++ Сhapter_5/Task_75.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    cout << "Целые числа от 100 до 200, кратные трем:\n";
+
+    int count = 0;
+    for (int i = 100; i <= 200; i++) {
+        if (i % 3 == 0) {
+            cout << i << " ";
+            count++;
+            if (count % 10 == 0) cout << endl; // по 10 чисел в строке
+        }
+    }
+
+    cout << "\nВсего чисел: " << count << endl;
+
+    return 0;
+}
