996a359d1d8bddebc7c6c7f899bfc528563bfa1c
Ivan <iv.vano77@mail.ru>
2025-11-28 10:41:44 +0300

    Add files via upload

---
+++ Сhapter_5/Task_83.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    cout << "Сумма положительных нечетных чисел, меньших 50:\n";
+
+    int sum = 0;
+    int count = 0;
+
+    for (int i = 1; i < 50; i += 2) {
+        sum += i;
+        count++;
+        cout << i;
+        if (i < 49) cout << " + ";
+    }
+
+    cout << " = " << sum << endl;
+    cout << "Количество слагаемых: " << count << endl;
+
+    return 0;
+}
