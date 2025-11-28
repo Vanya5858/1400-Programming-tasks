4ebd1006ae1ab29c9f0759137dc9174f68cbc904
Ivan <iv.vano77@mail.ru>
2025-11-28 10:39:22 +0300

    Add files via upload

---
+++ Сhapter_5/Task_71.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+    double result = 0.0;
+
+    // Вычисляем с конца для стабильности
+    for (int i = 50; i >= 1; i--) {
+        result = sqrt(i + result);
+    }
+
+    cout << "sqrt(1 + sqrt(2 + sqrt(3 + ... + sqrt(50)))) = " << result << endl;
+
+    return 0;
+}
