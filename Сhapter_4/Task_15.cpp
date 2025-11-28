3cb067ff3a6921f64f395fbe5bd53798e3c9b854
Ivan <iv.vano77@mail.ru>
2025-11-29 02:41:36 +0300

    Add files via upload

---
+++ Сhapter_4/Task_15.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, c;
+    cout << "Введите коэффициенты a, b, c: ";
+    cin >> a >> b >> c;
+
+    double D = b * b - 4 * a * c;
+
+    if (D >= 0) {
+        cout << "Уравнение имеет вещественные корни\n";
+    }
+    else {
+        cout << "Уравнение не имеет вещественных корней\n";
+    }
+
+    return 0;
+}
