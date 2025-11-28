8e374ece0b2b24975809ba40896402c8063396e2
Ivan <iv.vano77@mail.ru>
2025-11-29 02:40:41 +0300

    Add files via upload

---
+++ Сhapter_4/Task_6.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double x, y;
+    cout << "Введите x: ";
+    cin >> x;
+
+    // а) для графика а
+    if (x <= 2) {
+        y = x;
+    }
+    else {
+        y = 2;
+    }
+    cout << "а) y = " << y << endl;
+
+    // б) для графика б
+    if (x <= 3) {
+        y = -x;
+    }
+    else {
+        y = -3;
+    }
+    cout << "б) y = " << y << endl;
+
+    return 0;
+}
