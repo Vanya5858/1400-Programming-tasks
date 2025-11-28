a5f3d9c946a0f1e73dcd0b626ee4c9dbdc0f75bd
Ivan <iv.vano77@mail.ru>
2025-11-29 02:40:00 +0300

    Add files via upload

---
+++ Сhapter_4/Task_3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double x, y;
+    cout << "Введите x: ";
+    cin >> x;
+
+    if (x > 0) {
+        y = sin(x * x);
+    }
+    else {
+        y = 1 + 2 * sin(x) * sin(x);
+    }
+
+    cout << "y = " << y << endl;
+    return 0;
+}
