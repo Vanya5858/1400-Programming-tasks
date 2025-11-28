725a586d65142cb9cc03d9be27b5d6e4d7558686
Ivan <iv.vano77@mail.ru>
2025-11-28 10:42:06 +0300

    Add files via upload

---
+++ Сhapter_5/Task_85.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b;
+    cout << "Введите a: ";
+    cin >> a;
+    cout << "Введите b: ";
+    cin >> b;
+
+    int sum = 0;
+    for (int i = a; i <= b; i++) {
+        if (i % 4 == 0) {
+            sum += i;
+        }
+    }
+    cout << "Сумма чисел, кратных 4: " << sum << endl;
+    return 0;
+}
