56045d0250d3489af7bc6cf1eeaa1e2c804eb4fd
Ivan <iv.vano77@mail.ru>
2025-11-29 02:42:01 +0300

    Add files via upload

---
+++ Сhapter_4/Task_17.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int birth_year, birth_month, current_year, current_month;
+    cout << "Введите год и месяц рождения: ";
+    cin >> birth_year >> birth_month;
+    cout << "Введите текущий год и месяц: ";
+    cin >> current_year >> current_month;
+
+    int age = current_year - birth_year;
+    if (current_month < birth_month) {
+        age--;
+    }
+
+    cout << "Возраст: " << age << " лет\n";
+    return 0;
+}
