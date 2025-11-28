6664d96a0ec13053edca7814bed709ff97c932eb
Ivan <iv.vano77@mail.ru>
2025-11-28 10:40:21 +0300

    Add files via upload

---
+++ Сhapter_5/Task_76.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b, c;
+
+    cout << "Введите начало интервала a: ";
+    cin >> a;
+    cout << "Введите конец интервала b: ";
+    cin >> b;
+    cout << "Введите число c: ";
+    cin >> c;
+
+    cout << "Числа из интервала [" << a << ", " << b << "], кратные " << c << ":\n";
+
+    int count = 0;
+    // Находим первое число, кратное c, в интервале
+    int first_multiple = a;
+    if (a % c != 0) {
+        first_multiple = a + (c - a % c);
+    }
+
+    for (int i = first_multiple; i <= b; i += c) {
+        cout << i << " ";
+        count++;
+        if (count % 10 == 0) cout << endl;
+    }
+
+    cout << "\nВсего чисел: " << count << endl;
+
+    return 0;
+}
