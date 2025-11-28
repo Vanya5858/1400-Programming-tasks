7c324b26dcf5067d6b37338cc72393e8c491e065
Ivan <iv.vano77@mail.ru>
2025-11-29 02:41:09 +0300

    Add files via upload

---
+++ Сhapter_4/Task_8.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double x, k, f;
+    cout << "Введите x: ";
+    cin >> x;
+
+    if (sin(x) >= 0) {
+        k = x * x;
+    }
+    else {
+        k = fabs(x);
+    }
+
+    if (x < k) {
+        f = fabs(x);
+    }
+    else {
+        f = k * x;
+    }
+
+    cout << "f(x) = " << f << endl;
+    return 0;
+}
