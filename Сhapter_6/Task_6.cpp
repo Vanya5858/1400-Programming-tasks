310c70418676a9048334e297425442dc945ad998
Ivan <iv.vano77@mail.ru>
2025-11-30 23:52:30 +0300

    Add files via upload

---
+++ Сhapter_6/Task_6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    long long factorial;
+    cout << "Введите факториал: ";
+    cin >> factorial;
+
+    int n = 1;
+    long long product = 1;
+
+    while (product < factorial) {
+        n++;
+        product *= n;
+    }
+
+    if (product == factorial) {
+        cout << "Число: " << n << endl;
+    }
+    else {
+        cout << "Такого факториала не существует\n";
+    }
+
+    return 0;
+}
