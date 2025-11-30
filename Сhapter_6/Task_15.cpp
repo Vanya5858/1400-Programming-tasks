76e839b63238764285f9101406d2c2404ea7e65e
Ivan <iv.vano77@mail.ru>
2025-11-30 23:59:31 +0300

    Add files via upload

---
+++ Сhapter_6/Task_15.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    cout << "2n для n от 21 до 151 с шагом 10:\n";
+
+    for (int n = 21; n <= 151; n += 10) {
+        cout << 2 * n << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
