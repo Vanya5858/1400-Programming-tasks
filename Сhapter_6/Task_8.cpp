efbce3c5303996551deebc348c9c7378b630d786
Ivan <iv.vano77@mail.ru>
2025-11-30 23:53:14 +0300

    Add files via upload

---
+++ Сhapter_6/Task_8.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите n: ";
+    cin >> n;
+
+    int i = 1;
+    while (i * i <= n) {
+        i++;
+    }
+    cout << "Первое число, квадрат которого больше " << n << ": " << i << endl;
+
+    return 0;
+}
