e1c2a021e0cee509f83e7c17e6e1b32f4b5fa940
Ivan <iv.vano77@mail.ru>
2025-11-30 23:56:36 +0300

    Add files via upload

---
+++ Сhapter_6/Task_11.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int number;
+    int count = 0;
+
+    cout << "Введите 10 чисел (0 для досрочного завершения):\n";
+
+    while (count < 10) {
+        cin >> number;
+        if (number == 0) {
+            break;
+        }
+        count++;
+    }
+
+    cout << "Ввод завершен. Введено чисел: " << count << endl;
+
+    return 0;
+}
