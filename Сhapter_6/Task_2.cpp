dccac5458140df13ca034b77c3dd4180c9ccfac9
Ivan <iv.vano77@mail.ru>
2025-11-30 23:16:22 +0300

    Add files via upload

---
+++ Сhapter_6/Task_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите n: ";
+    cin >> n;
+
+    int i = 1;
+    while (i <= n) {
+        cout << i << " ";
+        i++;
+    }
+    cout << endl;
+
+    return 0;
+}
