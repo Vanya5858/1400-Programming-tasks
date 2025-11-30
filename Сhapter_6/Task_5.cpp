939d52a347c3fb20c10ca709c79d2d7180ec7970
Ivan <iv.vano77@mail.ru>
2025-11-30 23:52:16 +0300

    Add files via upload

---
+++ Сhapter_6/Task_5.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num = 5000;
+    while (num % 139 != 0) {
+        num--;
+    }
+    cout << "Максимальное число не превышающее 5000, делящееся на 139: " << num << endl;
+
+    return 0;
+}
