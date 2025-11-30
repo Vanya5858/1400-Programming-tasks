15aebc3b94827ce867dc85ab8167a888995d54f6
Ivan <iv.vano77@mail.ru>
2025-11-30 23:53:27 +0300

    Add files via upload

---
+++ Сhapter_6/Task_9.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int number;
+
+    do {
+        cout << "Введите четное число: ";
+        cin >> number;
+
+        if (number % 2 != 0) {
+            cout << "Ошибка! Число нечетное. Попробуйте снова.\n";
+        }
+    } while (number % 2 != 0);
+
+    cout << "Спасибо! Вы ввели четное число: " << number << endl;
+
+    return 0;
+}
