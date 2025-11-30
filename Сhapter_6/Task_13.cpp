d5fbba7021e1859890294944c4df38e31d7baa8c
Ivan <iv.vano77@mail.ru>
2025-11-30 23:58:04 +0300

    Add files via upload

---
+++ Сhapter_6/Task_13.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int choice;
+
+    switch (choice) {
+    case 1: {
+        // а) Цикл с предусловием
+        int i = 10;
+        while (i <= 30) {
+            cout << i << " ";
+            i++;
+        }
+        cout << endl;
+        cout << "Выполнено с предусловием (while)\n";
+        break;
+    }
+
+    case 2: {
+        // б) Цикл с постусловием
+        int i = 10;
+        do {
+            cout << i << " ";
+            i++;
+        } while (i <= 30);
+        cout << endl;
+        cout << "Выполнено с постусловием (do-while)\n";
+        break;
+    }
+
+    case 3: {
+        // в) Цикл с параметром
+        for (int i = 10; i <= 30; i++) {
+            cout << i << " ";
+        }
+        cout << endl;
+        cout << "Выполнено с параметром (for)\n";
+        break;
+    }
+
+    default:
+        cout << "Неверный выбор!\n";
+        return 1;
+    }
+
+    return 0;
+}
