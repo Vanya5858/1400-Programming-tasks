95694befd75bd1dce04b9dee502119051f007fff
Ivan <iv.vano77@mail.ru>
2025-11-30 23:51:59 +0300

    Add files via upload

---
+++ Сhapter_6/Task_4.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num = 191;
+    while (num % 17 != 0) {
+        num++;
+    }
+    cout << "Минимальное число больше 190, делящееся на 17: " << num << endl;
+
+    return 0;
+}
