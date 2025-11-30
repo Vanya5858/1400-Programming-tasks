7beb0dab2376039b9d1a88fac6d6e31ddae3d9ad
Ivan <iv.vano77@mail.ru>
2025-11-30 23:53:39 +0300

    Add files via upload

---
+++ Сhapter_6/Task_10.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const int CORRECT_PASSWORD = 12345; // Пример правильного пароля
+    int enteredPassword;
+
+    do {
+        cout << "Введите пароль: ";
+        cin >> enteredPassword;
+
+        if (enteredPassword != CORRECT_PASSWORD) {
+            cout << "Неверный пароль! Попробуйте снова.\n";
+        }
+    } while (enteredPassword != CORRECT_PASSWORD);
+
+    cout << "Добро пожаловать! Пароль верный.\n";
+
+    return 0;
+}
