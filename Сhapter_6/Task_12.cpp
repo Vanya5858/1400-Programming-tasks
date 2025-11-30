e71eb22ac76e3f6798b4f807959ffb62f4663a97
Ivan <iv.vano77@mail.ru>
2025-11-30 23:56:49 +0300

    Add files via upload

---
+++ Сhapter_6/Task_12.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int number;
+
+    cout << "Введите последовательность чисел (0 - конец):\n";
+
+    do {
+        cin >> number;
+        if (number != 0) {
+            cout << "Вы ввели число: " << number << endl;
+        }
+    } while (number != 0);
+
+    cout << "Конец последовательности\n";
+
+    return 0;
+}
