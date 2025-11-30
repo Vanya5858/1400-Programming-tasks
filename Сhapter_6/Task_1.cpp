1b81331420824ec7bb7fc7d8270f959fa23431b4
Ivan <iv.vano77@mail.ru>
2025-11-30 23:14:06 +0300

    Add files via upload

---
+++ Сhapter_6/Task_1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b;
+    cout << "Введите a и b (a > b): ";
+    cin >> a >> b;
+
+    // а) Целочисленное деление
+    int quotient = 0;
+    int temp = a;
+    while (temp >= b) {
+        temp -= b;
+        quotient++;
+    }
+    cout << "Целочисленное деление: " << quotient << endl;
+
+    // б) Остаток от деления
+    int remainder = a;
+    while (remainder >= b) {
+        remainder -= b;
+    }
+    cout << "Остаток от деления: " << remainder << endl;
+
+    return 0;
+}
