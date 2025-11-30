88cf5b4cfd2099008d3b1d11c0903c25fed600f6
Ivan <iv.vano77@mail.ru>
2025-11-30 23:59:20 +0300

    Add files via upload

---
+++ Сhapter_6/Task_14.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int i = 100;
+
+    cout << "Числа от 100 до 80:\n";
+    while (i >= 80) {
+        cout << i << " ";
+        i--;
+    }
+    cout << endl;
+
+    return 0;
+}
