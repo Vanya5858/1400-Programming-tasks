d10f75f25db3f9c7a6fd353bb5a0e5115cc19f71
Ivan <iv.vano77@mail.ru>
2025-12-02 23:00:13 +0300

    Add files via upload

---
+++ Сhapter_4/Task_61.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, c;
+    cout << "Введите коэффициенты a, b, c (a != 0): ";
+    cin >> a >> b >> c;
+
+    double D = b * b - 4 * a * c;
+
+    if (D > 0) {
+        double x1 = (-b + sqrt(D)) / (2 * a);
+        double x2 = (-b - sqrt(D)) / (2 * a);
+        cout << "Два различных корня: x1 = " << x1 << ", x2 = " << x2 << endl;
+    }
+    else if (D == 0) {
+        double x = -b / (2 * a);
+        cout << "Один корень (кратности 2): x = " << x << endl;
+    }
+    else {
+        cout << "Действительных корней нет\n";
+    }
+
+    return 0;
+}
