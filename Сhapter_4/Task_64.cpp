6e613c385dd2e23f99850cb53a30b0ae88827a59
Ivan <iv.vano77@mail.ru>
2025-12-02 23:00:55 +0300

    Add files via upload

---
+++ Сhapter_4/Task_64.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, d;
+    cout << "Введите размеры форточки (a b) в см: ";
+    cin >> a >> b;
+    cout << "Введите диаметр головы в см: ";
+    cin >> d;
+
+    // Учитываем зазор 1 см с каждой стороны
+    if (d + 2 <= a && d + 2 <= b) {
+        cout << "Вася сможет высунуть голову\n";
+    }
+    else {
+        cout << "Вася не сможет высунуть голову\n";
+    }
+
+    return 0;
+}
