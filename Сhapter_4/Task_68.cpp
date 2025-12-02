a0b0e0141ee2c3b61f32080f36d27449287c2fc7
Ivan <iv.vano77@mail.ru>
2025-12-02 23:01:48 +0300

    Add files via upload

---
+++ Сhapter_4/Task_68.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int year;
+    cout << "Введите год: ";
+    cin >> year;
+
+    bool is_leap = false;
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                is_leap = true;
+            }
+        }
+        else {
+            is_leap = true;
+        }
+    }
+
+    if (is_leap) {
+        cout << year << " год високосный\n";
+    }
+    else {
+        cout << year << " год не високосный\n";
+    }
+
+    return 0;
+}
