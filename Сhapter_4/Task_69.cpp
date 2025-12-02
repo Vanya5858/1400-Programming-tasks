6a68e7ae994cc46b1be00e0034f1e4d572f9f867
Ivan <iv.vano77@mail.ru>
2025-12-02 23:01:59 +0300

    Add files via upload

---
+++ Сhapter_4/Task_69.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b, c, d, e;
+    cout << "Введите размеры стола (a b): ";
+    cin >> a >> b;
+    cout << "Введите размеры кости домино (c d e): ";
+    cin >> c >> d >> e;
+
+    // Рассчитываем количество костей для разных ориентаций
+    int count1 = (a / c) * (b / d);
+    int count2 = (a / d) * (b / c);
+
+    cout << "При ориентации c*d: " << count1 << " костей\n";
+    cout << "При ориентации d*c: " << count2 << " костей\n";
+    cout << "Максимальное количество: " << max(count1, count2) << endl;
+
+    return 0;
+}
