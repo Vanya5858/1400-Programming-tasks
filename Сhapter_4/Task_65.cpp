6fee8db3261c055753b4bf6ad546a79b447321b1
Ivan <iv.vano77@mail.ru>
2025-12-02 23:01:07 +0300

    Add files via upload

---
+++ Сhapter_4/Task_65.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, c, x, y;
+    cout << "Введите размеры кирпича (a b c): ";
+    cin >> a >> b >> c;
+    cout << "Введите размеры отверстия (x y): ";
+    cin >> x >> y;
+
+    // Проверяем все возможные ориентации кирпича
+    if ((a <= x && b <= y) || (a <= y && b <= x) ||
+        (a <= x && c <= y) || (a <= y && c <= x) ||
+        (b <= x && c <= y) || (b <= y && c <= x)) {
+        cout << "Кирпич пройдет в отверстие\n";
+    }
+    else {
+        cout << "Кирпич не пройдет в отверстие\n";
+    }
+
+    return 0;
+}
