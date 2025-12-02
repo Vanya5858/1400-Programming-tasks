a02d9b5a761dbfd0150e0476b762834092d91368
Ivan <iv.vano77@mail.ru>
2025-12-02 23:00:41 +0300

    Add files via upload

---
+++ Сhapter_4/Task_63.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, c, d;
+    cout << "Введите размеры конверта (a b) в мм: ";
+    cin >> a >> b;
+    cout << "Введите размеры открытки (c d) в мм: ";
+    cin >> c >> d;
+
+    // Учитываем зазор 1 мм с каждой стороны
+    if ((c + 2 <= a && d + 2 <= b) || (c + 2 <= b && d + 2 <= a)) {
+        cout << "Открытка помещается в конверт\n";
+    }
+    else {
+        cout << "Открытка не помещается в конверт\n";
+    }
+
+    return 0;
+}
