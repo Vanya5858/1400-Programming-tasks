114dceec5361bdc7a9c79231f4cce2e33dbcd09f
Ivan <iv.vano77@mail.ru>
2025-12-03 20:09:23 +0300

    Add files via upload

---
+++ Сhapter_4/Task_94.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int k;
+    cout << "Введите k (1-180): ";
+    cin >> k;
+
+    int position = k;
+    int number = 10;
+
+    while (position > 2) {
+        position -= 2;
+        number++;
+    }
+
+    if (position == 1) {
+        cout << "k-я цифра: " << number / 10 << endl;
+    }
+    else {
+        cout << "k-я цифра: " << number % 10 << endl;
+    }
+
+    return 0;
+}
