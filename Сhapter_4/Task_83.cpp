76735c2d20bd111f896d960d528b7a40c8eb3e18
Ivan <iv.vano77@mail.ru>
2025-12-03 20:02:54 +0300

    Add files via upload

---
+++ Сhapter_4/Task_83.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int k;
+    cout << "Введите количество грибов: ";
+    cin >> k;
+
+    cout << "мы нашли " << k << " гриб";
+
+    if (k % 10 == 1 && k != 11) {
+        // ничего не добавляем
+    }
+    else if (k % 10 >= 2 && k % 10 <= 4 && (k < 10 || k > 20)) {
+        cout << "а";
+    }
+    else {
+        cout << "ов";
+    }
+    cout << " в лесу\n";
+
+    return 0;
+}
