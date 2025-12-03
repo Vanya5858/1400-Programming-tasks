f17b19147ab78b368690de31b86ccf08a5b085d8
Ivan <iv.vano77@mail.ru>
2025-12-03 20:08:40 +0300

    Add files via upload

---
+++ Сhapter_4/Task_92.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double t;
+    cout << "Введите время в минутах с начала часа: ";
+    cin >> t;
+
+    // Цикл: 3 мин зеленый, 1 мин желтый, 2 мин красный
+    int cycle_time = (int)t % 6;
+
+    if (cycle_time < 3) {
+        cout << "Горит зеленый сигнал\n";
+    }
+    else if (cycle_time < 4) {
+        cout << "Горит желтый сигнал\n";
+    }
+    else {
+        cout << "Горит красный сигнал\n";
+    }
+
+    return 0;
+}
