2dbc779502a8f652da97bd4fb93907737dfbbdf1
Ivan <iv.vano77@mail.ru>
2025-12-04 12:51:49 +0300

    Add files via upload

---
+++ Сhapter_4/Task_112.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b, c;
+    cout << "Введите три целых числа: ";
+    cin >> a >> b >> c;
+
+    int sum = 0;
+    if (a > 0) sum += a;
+    if (b > 0) sum += b;
+    if (c > 0) sum += c;
+
+    cout << "Сумма положительных чисел: " << sum << endl;
+
+    return 0;
+}
