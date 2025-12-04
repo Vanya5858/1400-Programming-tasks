e43e8eb4980745af41621783a9e1f01287ada395
Ivan <iv.vano77@mail.ru>
2025-12-04 12:50:40 +0300

    Add files via upload

---
+++ Сhapter_4/Task_110.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, c, d;
+    cout << "Введите четыре вещественных числа: ";
+    cin >> a >> b >> c >> d;
+
+    int count = 0;
+    if (a < 0) count++;
+    if (b < 0) count++;
+    if (c < 0) count++;
+    if (d < 0) count++;
+
+    cout << "Количество отрицательных чисел: " << count << endl;
+
+    return 0;
+}
