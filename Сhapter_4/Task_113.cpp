4d9a85ca068f63ae0b4202babcae479fd797149f
Ivan <iv.vano77@mail.ru>
2025-12-04 12:52:10 +0300

    Add files via upload

---
+++ Сhapter_4/Task_113.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, c, d;
+    cout << "Введите четыре вещественных числа: ";
+    cin >> a >> b >> c >> d;
+
+    double sum = 0;
+    if (a > 5) sum += a;
+    if (b > 5) sum += b;
+    if (c > 5) sum += c;
+    if (d > 5) sum += d;
+
+    cout << "Сумма чисел больше пяти: " << sum << endl;
+
+    return 0;
+}
