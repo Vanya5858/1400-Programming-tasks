f1bd7f9cba500a6395d54f2b41a832d197ef3095
Ivan <iv.vano77@mail.ru>
2025-12-04 12:49:47 +0300

    Add files via upload

---
+++ Сhapter_4/Task_106.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+
+    if (sqrt(b) < a) {
+        b *= 5;
+        cout << "Второе число увеличено в 5 раз: " << b << endl;
+    }
+
+    return 0;
+}
