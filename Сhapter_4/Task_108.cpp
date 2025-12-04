7e96737b401ecf1e59f143b6571878d8d2937b4b
Ivan <iv.vano77@mail.ru>
2025-12-04 12:50:11 +0300

    Add files via upload

---
+++ Сhapter_4/Task_108.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, c;
+    cout << "Введите три вещественных числа: ";
+    cin >> a >> b >> c;
+
+    if (a >= 0) a = a * a;
+    if (b >= 0) b = b * b;
+    if (c >= 0) c = c * c;
+
+    cout << "После возведения в квадрат: " << a << " " << b << " " << c << endl;
+
+    return 0;
+}
