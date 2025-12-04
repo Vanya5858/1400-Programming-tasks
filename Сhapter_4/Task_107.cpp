17435cebdd2f98053b930d9c425641c124851369
Ivan <iv.vano77@mail.ru>
2025-12-04 12:49:58 +0300

    Add files via upload

---
+++ Сhapter_4/Task_107.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b, c;
+    cout << "Введите три целых числа: ";
+    cin >> a >> b >> c;
+
+    cout << "Четные числа: ";
+    if (a % 2 == 0) cout << a << " ";
+    if (b % 2 == 0) cout << b << " ";
+    if (c % 2 == 0) cout << c << " ";
+    cout << endl;
+
+    return 0;
+}
