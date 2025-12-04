e7378c05805fb99187642cdde97ff001dccea237
Ivan <iv.vano77@mail.ru>
2025-12-04 12:49:20 +0300

    Add files via upload

---
+++ Сhapter_4/Task_104.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+
+    // а) полусумма абсолютных величин
+    double abs_a = a, abs_b = b;
+    if (a < 0) abs_a = -a;
+    if (b < 0) abs_b = -b;
+
+    cout << "а) Полусумма: " << (abs_a + abs_b) / 2 << endl;
+
+    // б) квадратный корень из произведения абсолютных величин
+    cout << "б) Корень из произведения: " << sqrt(abs_a * abs_b) << endl;
+
+    return 0;
+}
