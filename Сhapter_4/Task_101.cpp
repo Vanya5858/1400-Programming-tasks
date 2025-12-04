58f0b249b26c924f7bdfd2e94094ce3a25cff8d2
Ivan <iv.vano77@mail.ru>
2025-12-04 12:48:15 +0300

    Add files via upload

---
+++ Сhapter_4/Task_101.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, c;
+    cout << "Введите три различных числа: ";
+    cin >> a >> b >> c;
+
+    // а) наибольшее
+    double max_val = a;
+    if (b > max_val) max_val = b;
+    if (c > max_val) max_val = c;
+    cout << "а) Наибольшее: " << max_val << endl;
+
+    // б) наименьшее
+    double min_val = a;
+    if (b < min_val) min_val = b;
+    if (c < min_val) min_val = c;
+    cout << "б) Наименьшее: " << min_val << endl;
+
+    return 0;
+}
