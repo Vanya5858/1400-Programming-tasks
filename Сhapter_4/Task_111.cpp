b8a9b3d7347b7c8961ee7a9374bf943ee3c10fa8
Ivan <iv.vano77@mail.ru>
2025-12-04 12:51:33 +0300

    Add files via upload

---
+++ Сhapter_4/Task_111.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b, c, d;
+    cout << "Введите четыре целых числа: ";
+    cin >> a >> b >> c >> d;
+
+    int count = 0;
+    if (a % 2 == 0) count++;
+    if (b % 2 == 0) count++;
+    if (c % 2 == 0) count++;
+    if (d % 2 == 0) count++;
+
+    cout << "Количество четных чисел: " << count << endl;
+
+    return 0;
+}
