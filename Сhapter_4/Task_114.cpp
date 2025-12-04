87e6adebbe0aaa39d1dff2157de5e64958d7fd50
Ivan <iv.vano77@mail.ru>
2025-12-04 12:52:52 +0300

    Add files via upload

---
+++ Сhapter_4/Task_114.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, b, c, d;
+    cout << "Введите четыре целых числа: ";
+    cin >> a >> b >> c >> d;
+
+    int sum = 0;
+    if (a % 3 == 0) sum += a;
+    if (b % 3 == 0) sum += b;
+    if (c % 3 == 0) sum += c;
+    if (d % 3 == 0) sum += d;
+
+    cout << "Сумма чисел, кратных трем: " << sum << endl;
+
+    return 0;
+}
