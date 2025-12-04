efa2886206ee6c9ca4dd54c6f919931c7b6a571a
Ivan <iv.vano77@mail.ru>
2025-12-04 12:49:35 +0300

    Add files via upload

---
+++ Сhapter_4/Task_105.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+
+    // Приводим к абсолютным величинам
+    double abs_a = a, abs_b = b;
+    if (a < 0) abs_a = -a;
+    if (b < 0) abs_b = -b;
+
+    if (abs_a > abs_b) {
+        a /= 2;
+        cout << "Первое число уменьшено в 2 раза: " << a << endl;
+    }
+
+    return 0;
+}
