553df4d48f62b6a69f5afbd9197a56f37941af4d
Ivan <iv.vano77@mail.ru>
2025-12-04 12:48:54 +0300

    Add files via upload

---
+++ Сhapter_4/Task_103.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double x;
+    cout << "Введите число: ";
+    cin >> x;
+
+    double abs_val = x;
+    if (x < 0) abs_val = -x;
+
+    cout << "Абсолютная величина: " << abs_val << endl;
+
+    return 0;
+}
