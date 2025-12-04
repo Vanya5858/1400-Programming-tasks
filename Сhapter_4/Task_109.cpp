e9bcd841fc211c710c8777fe04b5d3797d18e7ac
Ivan <iv.vano77@mail.ru>
2025-12-04 12:50:27 +0300

    Add files via upload

---
+++ Сhapter_4/Task_109.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, b, c;
+    cout << "Введите три вещественных числа: ";
+    cin >> a >> b >> c;
+
+    cout << "Числа в интервале [1.6, 3.8]: ";
+    if (a >= 1.6 && a <= 3.8) cout << a << " ";
+    if (b >= 1.6 && b <= 3.8) cout << b << " ";
+    if (c >= 1.6 && c <= 3.8) cout << c << " ";
+    cout << endl;
+
+    return 0;
+}
