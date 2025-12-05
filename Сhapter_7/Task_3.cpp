7999241bfc26aa234d9378e2fbb376a59d3e11d9
Ivan <iv.vano77@mail.ru>
2025-12-05 09:18:03 +0300

    Add files via upload

---
+++ Сhapter_7/Task_3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double side, perimeter = 0;
+
+    cout << "Введите длины 12 сторон 12-угольника: ";
+    for (int i = 0; i < 12; i++) {
+        cin >> side;
+        perimeter += side;
+    }
+
+    cout << "Периметр = " << perimeter;
+    return 0;
+}
