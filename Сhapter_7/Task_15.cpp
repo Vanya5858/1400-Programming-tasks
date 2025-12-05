3bbfb451fbaf278d7857a7be66694c7f83edbd14
Ivan <iv.vano77@mail.ru>
2025-12-05 23:51:13 +0300

    Add files via upload

---
+++ Сhapter_7/Task_15.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, sum = 0;
+
+    cout << "Введите 10 вещественных чисел: ";
+    for (int i = 0; i < 10; i++) {
+        cin >> a;
+        sum += a;
+    }
+
+    cout << (sum > 100.78 ? "Сумма превышает 100.78" : "Сумма не превышает 100.78");
+    return 0;
+}
