c0a5d32cc0675ab14dbef21a1c1cc90821e9e7d9
Ivan <iv.vano77@mail.ru>
2025-12-05 09:20:18 +0300

    Add files via upload

---
+++ Сhapter_7/Task_12.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, schet = 1;
+    cout << "Введите последовательность положительных чисел (оканчивается 0): ";
+
+    while (true) {
+        cin >> num;
+        if (num == 0) break;
+        schet *= num;
+        cout << schet << endl;
+    }
+
+    cout << "0";
+    return 0;
+}
