97f6a2b2ff512355a9150a8c2cd6ff09b553a213
Ivan <iv.vano77@mail.ru>
2025-12-05 09:19:45 +0300

    Add files via upload

---
+++ Сhapter_7/Task_10.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double score, sum1 = 0, sum2 = 0;
+
+    cout << "Введите результаты первого пятиборца (5 видов): ";
+    for (int i = 0; i < 5; i++) {
+        cin >> score;
+        sum1 += score;
+    }
+
+    cout << "Введите результаты второго пятиборца (5 видов): ";
+    for (int i = 0; i < 5; i++) {
+        cin >> score;
+        sum2 += score;
+    }
+
+    cout << "Сумма баллов первого спортсмена = " << sum1 << endl;
+    cout << "Сумма баллов второго спортсмена = " << sum2;
+    return 0;
+}
