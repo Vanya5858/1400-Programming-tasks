30ac5017e14f987a4443a5a6ffc3e9cdf46299ee
Ivan <iv.vano77@mail.ru>
2025-12-05 09:17:33 +0300

    Add files via upload

---
+++ Сhapter_7/Task_1.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, sum = 0;
+
+    cout << "Введите 10 чисел: ";
+    for (int i = 0; i < 10; i++) {
+        cin >> a;
+        sum += a;
+    }
+
+    cout << "Сумма = " << sum;
+    return 0;
+}
