dc788130e38c14bef7bd79f2af4f3bc93df944d2
Ivan <iv.vano77@mail.ru>
2025-12-05 09:18:17 +0300

    Add files via upload

---
+++ Сhapter_7/Task_4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double mass, total = 0;
+    int count;
+
+    cout << "Введите количество предметов: ";
+    cin >> count;
+    cout << "Введите массу каждого предмета: ";
+
+    for (int i = 0; i < count; i++) {
+        cin >> mass;
+        total += mass;
+    }
+
+    cout << "Общая масса = " << total;
+    return 0;
+}
