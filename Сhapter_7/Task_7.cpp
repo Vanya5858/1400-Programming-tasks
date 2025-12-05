1c51bca00d80b31bdbd435fdd04a33eaf8d88a17
Ivan <iv.vano77@mail.ru>
2025-12-05 09:19:07 +0300

    Add files via upload

---
+++ Сhapter_7/Task_7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double r, total = 0;
+    int n;
+
+    cout << "Введите количество элементов: ";
+    cin >> n;
+    cout << "Введите сопротивление каждого: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> r;
+        total += 1 / r;
+    }
+
+    cout << "Общее сопротивление (параллельно) = " << 1 / total;
+    return 0;
+}
