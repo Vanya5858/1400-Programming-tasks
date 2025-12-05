bcfc1e4b6206891cb870cd6d8a716fbb13d18364
Ivan <iv.vano77@mail.ru>
2025-12-05 09:18:36 +0300

    Add files via upload

---
+++ Сhapter_7/Task_5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double salary, total = 0;
+    int employees;
+
+    cout << "Введите количество сотрудников: ";
+    cin >> employees;
+    cout << "Введите зарплату каждого: ";
+
+    for (int i = 0; i < employees; i++) {
+        cin >> salary;
+        total += salary;
+    }
+
+    cout << "Общая сумма выплат = " << total;
+    return 0;
+}
