56598f56f749a5353746ce8becb65da15e37d998
Ivan <iv.vano77@mail.ru>
2025-12-06 23:20:19 +0300

    Add files via upload

---
+++ Сhapter_7/Task_29.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int students, grade, sum = 0;
+
+    cout << "Введите количество учеников в классе: ";
+    cin >> students;
+    cout << "Введите оценки по алгебре для каждого ученика: ";
+
+    for (int i = 0; i < students; i++) {
+        cin >> grade;
+        sum += grade;
+    }
+
+    cout << "Средняя оценка = " << sum / (double)students;
+    return 0;
+}
