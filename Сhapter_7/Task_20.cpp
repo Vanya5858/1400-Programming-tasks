89ecc687999a24faf134bfef9c6c2ea0c624c069
Ivan <iv.vano77@mail.ru>
2025-12-06 23:18:34 +0300

    Add files via upload

---
+++ Сhapter_7/Task_20.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    double mass, total = 0;
+
+    cout << "Введите грузоподъёмность автомобиля (кг): ";
+    double capacity;
+    cin >> capacity;
+
+    cout << "Введите количество грузов: ";
+    cin >> n;
+    cout << "Введите массу каждого груза: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> mass;
+        total += mass;
+    }
+
+    cout << (total <= capacity ? "Не превысило" : "Превысило");
+    return 0;
+}
