5c50147f2a7af2875cbcad565eaa9c00e5862039
Ivan <iv.vano77@mail.ru>
2025-12-06 23:19:44 +0300

    Add files via upload

---
+++ Сhapter_7/Task_26.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    double a, sum = 0;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    cout << "Введите " << n << " вещественных чисел: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        sum += a;
+    }
+
+    cout << "Среднее арифметическое = " << sum / n;
+    return 0;
+}
