2191af8747494e8d0d6a94d8447c756f81e3fceb
Ivan <iv.vano77@mail.ru>
2025-12-06 23:18:23 +0300

    Add files via upload

---
+++ Сhapter_7/Task_19.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double precipitation, total = 0;
+
+    cout << "Введите количество осадков за каждый день февраля (28 чисел): ";
+    for (int i = 0; i < 28; i++) {
+        cin >> precipitation;
+        total += precipitation;
+    }
+
+    double lastYear;
+    cout << "Введите общее количество осадков за прошлый год: ";
+    cin >> lastYear;
+
+    cout << (total > lastYear ? "Превысило" : "Не превысило");
+    return 0;
+}
