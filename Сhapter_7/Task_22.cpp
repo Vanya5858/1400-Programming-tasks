2e3338d6ebcd8745cdb5d21abf99de6e1b1f97bb
Ivan <iv.vano77@mail.ru>
2025-12-06 23:18:59 +0300

    Add files via upload

---
+++ Сhapter_7/Task_22.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double price1, price2, sum1 = 0, sum2 = 0;
+
+    cout << "Введите стоимость 8 предметов из первого набора: ";
+    for (int i = 0; i < 8; i++) {
+        cin >> price1;
+        sum1 += price1;
+    }
+
+    cout << "Введите стоимость 8 предметов из второго набора: ";
+    for (int i = 0; i < 8; i++) {
+        cin >> price2;
+        sum2 += price2;
+    }
+
+    if (sum1 < sum2)
+        cout << "Первый набор дешевле";
+    else if (sum2 < sum1)
+        cout << "Второй набор дешевле";
+    else
+        cout << "Наборы стоят одинаково";
+
+    return 0;
+}
