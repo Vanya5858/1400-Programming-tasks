a85284e0553735a895f1a6632c6cbd696d28d84f
Ivan <iv.vano77@mail.ru>
2025-12-06 23:18:44 +0300

    Add files via upload

---
+++ Сhapter_7/Task_21.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double score1, score2, total1 = 0, total2 = 0;
+
+    cout << "Введите результаты первого десятиборца (10 видов): ";
+    for (int i = 0; i < 10; i++) {
+        cin >> score1;
+        total1 += score1;
+    }
+
+    cout << "Введите результаты второго десятиборца (10 видов): ";
+    for (int i = 0; i < 10; i++) {
+        cin >> score2;
+        total2 += score2;
+    }
+
+    if (total1 > total2)
+        cout << "Первый спортсмен показал лучший результат";
+    else if (total2 > total1)
+        cout << "Второй спортсмен показал лучший результат";
+    else
+        cout << "Результаты равны";
+
+    return 0;
+}
