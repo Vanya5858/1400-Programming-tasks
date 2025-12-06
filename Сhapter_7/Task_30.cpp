05f528bd05c208d573e4b5ffad253d44122ddd4d
Ivan <iv.vano77@mail.ru>
2025-12-06 23:20:30 +0300

    Add files via upload

---
+++ Сhapter_7/Task_30.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    double mass, sum = 0;
+
+    cout << "Введите количество предметов: ";
+    cin >> n;
+    cout << "Введите массу каждого предмета: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> mass;
+        sum += mass;
+    }
+
+    cout << "Средняя масса = " << sum / n;
+    return 0;
+}
