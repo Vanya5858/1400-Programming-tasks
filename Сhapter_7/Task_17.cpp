671188e0eee5cf7fac9673b7156538cbb0ba6f41
Ivan <iv.vano77@mail.ru>
2025-12-06 23:18:00 +0300

    Add files via upload

---
+++ Сhapter_7/Task_17.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, sum = 0;
+
+    cout << "Введите 9 целых чисел: ";
+    for (int i = 0; i < 9; i++) {
+        cin >> a;
+        sum += a;
+    }
+
+    cout << (sum % 2 == 0 ? "Сумма чётная" : "Сумма нечётная");
+    return 0;
+}
