a7bdee322cc663881a7be7a61e7e4f001eec75a9
Ivan <iv.vano77@mail.ru>
2025-12-06 23:18:12 +0300

    Add files via upload

---
+++ Сhapter_7/Task_18.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n, x, sum = 0;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    cout << "Введите " << n << " целых чисел: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        sum += x;
+    }
+
+    int b;
+    cout << "Введите число b: ";
+    cin >> b;
+
+    cout << (sum % b == 0 ? "Сумма кратна b" : "Сумма не кратна b");
+    return 0;
+}
