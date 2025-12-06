644e8689a557e1dcbf4173c7a70b8be133cefc85
Ivan <iv.vano77@mail.ru>
2025-12-06 23:19:13 +0300

    Add files via upload

---
+++ Сhapter_7/Task_23.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, product = 1;
+
+    cout << "Введите 8 чисел: ";
+    for (int i = 0; i < 8; i++) {
+        cin >> a;
+        product *= a;
+    }
+
+    cout << (product < 10000 ? "Произведение меньше 10000" : "Произведение не меньше 10000");
+    return 0;
+}
