d49ed6aeee6a9e9ae5aef84d787a4f3cdaeee1bb
Ivan <iv.vano77@mail.ru>
2025-12-06 23:20:41 +0300

    Add files via upload

---
+++ Сhapter_7/Task_31.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, count = 0, sum = 0;
+
+    cout << "Введите последовательность неотрицательных чисел (оканчивается отрицательным): ";
+
+    while (true) {
+        cin >> num;
+        if (num < 0) break;
+        sum += num;
+        count++;
+    }
+
+    if (count > 0)
+        cout << "Среднее арифметическое = " << (double)sum / count;
+    else
+        cout << "Чисел не было введено";
+
+    return 0;
+}
