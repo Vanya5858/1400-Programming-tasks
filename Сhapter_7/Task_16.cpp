bd782f563aaba173f2885224bf1bb7d1b5935331
Ivan <iv.vano77@mail.ru>
2025-12-06 23:17:42 +0300

    Add files via upload

---
+++ Сhapter_7/Task_16.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n, b, sum = 0;
+
+    cout << "Введите число n: ";
+    cin >> n;
+
+    cout << "Введите вашу последовательность из 10 чисел через пробел: ";
+
+    for (int i = 0; i < 10; i++) {
+        cin >> b;
+        sum += b;
+    }
+
+    cout << (sum < n ? "Сумма меньше n" : "Сумма не меньше n");
+    return 0;
+}
