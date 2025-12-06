36a3975b14c72d1532dd5997836dce7fef06f11c
Ivan <iv.vano77@mail.ru>
2025-12-06 23:19:24 +0300

    Add files via upload

---
+++ Сhapter_7/Task_24.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    double d, product = 1;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    cout << "Введите " << n << " вещественных чисел: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> d;
+        product *= d;
+    }
+
+    double s;
+    cout << "Введите число s для сравнения: ";
+    cin >> s;
+
+    cout << (product > s ? "Произведение больше s" : "Произведение не больше s");
+    return 0;
+}
