17d55621b5e72e8d44be191a1230caba516e7a03
Ivan <iv.vano77@mail.ru>
2025-12-07 23:11:12 +0300

    Add files via upload

---
+++ Сhapter_7/Task_37.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double population, area, totalPopulation = 0, totalArea = 0;
+
+    cout << "Введите данные по 12 районам (население в тыс., площадь в км^2):\n";
+
+    for (int i = 0; i < 12; i++) {
+        cin >> population >> area;
+        totalPopulation += population;
+        totalArea += area;
+    }
+
+    cout << "Средняя плотность населения по области = "
+        << totalPopulation / totalArea << " тыс. чел./км^2";
+
+    return 0;
+}
