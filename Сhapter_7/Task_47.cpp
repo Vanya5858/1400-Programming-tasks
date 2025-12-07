4d133b21eed751071f8adb68cbfe5b4ff27398e0
Ivan <iv.vano77@mail.ru>
2025-12-07 23:13:18 +0300

    Add files via upload

---
+++ Сhapter_7/Task_47.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double price, total = 0;
+
+    cout << "Введите цены товаров (завершите ввод числом 0 или отрицательным): ";
+
+    while (true) {
+        cin >> price;
+        if (price <= 0) break;
+        if (price > 1000)
+            total += price;
+    }
+
+    cout << "Общая стоимость товаров дороже 1000 рублей = " << total;
+    return 0;
+}
