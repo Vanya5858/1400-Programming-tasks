72b7f3853f2352d7dd7bf0dc0728807c349c676b
Ivan <iv.vano77@mail.ru>
2025-12-07 23:11:46 +0300

    Add files via upload

---
+++ Сhapter_7/Task_40.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a, sum = 0;
+
+    cout << "Введите 12 вещественных чисел: ";
+    for (int i = 0; i < 12; i++) {
+        cin >> a;
+        if (a > 10.75)
+            sum += a;
+    }
+
+    cout << "Сумма чисел больше 10.75 = " << sum;
+    return 0;
+}
