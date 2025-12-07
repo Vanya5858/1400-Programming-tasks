e8ba40cd0316c177af5e6dd33d392a6856aa97de
Ivan <iv.vano77@mail.ru>
2025-12-07 23:12:46 +0300

    Add files via upload

---
+++ Сhapter_7/Task_45.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double c, sum = 0;
+
+    cout << "Введите 15 вещественных чисел: ";
+    for (int i = 1; i <= 15; i++) {
+        cin >> c;
+        if (i % 2 == 1)
+            sum -= c;
+    }
+
+    cout << "-c1 - c3 - c5 - ... = " << sum;
+    return 0;
+}
