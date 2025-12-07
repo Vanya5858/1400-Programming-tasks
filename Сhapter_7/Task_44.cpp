8d572baecef68bd77e2898fcf5df22b979cd174c
Ivan <iv.vano77@mail.ru>
2025-12-07 23:12:36 +0300

    Add files via upload

---
+++ Сhapter_7/Task_44.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, sum = 0;
+
+    cout << "Введите 20 целых чисел: ";
+    for (int i = 1; i <= 20; i++) {
+        cin >> a;
+        if (i % 2 == 0)
+            sum += a;
+    }
+
+    cout << "Сумма элементов на чётных позициях = " << sum;
+    return 0;
+}
