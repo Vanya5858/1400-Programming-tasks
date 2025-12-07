3ae766a087108a159d7b67618b528c6aba04e355
Ivan <iv.vano77@mail.ru>
2025-12-07 23:10:15 +0300

    Add files via upload

---
+++ Сhapter_7/Task_33.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double precip, sumJan = 0, sumMar = 0;
+
+    cout << "Введите количество осадков за каждый день января (31 число): ";
+    for (int i = 0; i < 31; i++) {
+        cin >> precip;
+        sumJan += precip;
+    }
+
+    cout << "Введите количество осадков за каждый день марта (31 число): ";
+    for (int i = 0; i < 31; i++) {
+        cin >> precip;
+        sumMar += precip;
+    }
+
+    cout << "Среднедневное количество осадков в январе = " << sumJan / 31 << endl;
+    cout << "Среднедневное количество осадков в марте = " << sumMar / 31;
+
+    return 0;
+}
