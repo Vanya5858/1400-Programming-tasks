1c298424203268eeb676cabd0b3f82393a6a5a81
Ivan <iv.vano77@mail.ru>
2025-12-07 23:09:58 +0300

    Add files via upload

---
+++ Сhapter_7/Task_32.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double age, sum1 = 0, sum2 = 0;
+
+    cout << "Введите возраст 20 учеников первого класса: ";
+    for (int i = 0; i < 20; i++) {
+        cin >> age;
+        sum1 += age;
+    }
+
+    cout << "Введите возраст 20 учеников второго класса: ";
+    for (int i = 0; i < 20; i++) {
+        cin >> age;
+        sum2 += age;
+    }
+
+    cout << "Средний возраст первого класса = " << sum1 / 20.0 << endl;
+    cout << "Средний возраст второго класса = " << sum2 / 20.0;
+
+    return 0;
+}
