23c54d52e56ab3fed5c033dc252797e71449dbd0
Ivan <iv.vano77@mail.ru>
2025-12-07 23:12:08 +0300

    Add files via upload

---
+++ Сhapter_7/Task_41.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    double b, p, sum = 0;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    cout << "Введите значение p: ";
+    cin >> p;
+    cout << "Введите " << n << " вещественных чисел: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> b;
+        if (b > p)
+            sum += b;
+    }
+
+    cout << "Сумма чисел больше " << p << " => " << sum;
+    return 0;
+}
