a6a70d7e5b006d650fe411475d4494c1fe7c7383
Ivan <iv.vano77@mail.ru>
2025-12-07 23:10:27 +0300

    Add files via upload

---
+++ Сhapter_7/Task_34.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double height, sum1 = 0, sum2 = 0;
+    int students;
+
+    cout << "Введите количество учеников в каждом классе: ";
+    cin >> students;
+
+    cout << "Введите рост учеников первого класса: ";
+    for (int i = 0; i < students; i++) {
+        cin >> height;
+        sum1 += height;
+    }
+
+    cout << "Введите рост учеников второго класса: ";
+    for (int i = 0; i < students; i++) {
+        cin >> height;
+        sum2 += height;
+    }
+
+    cout << "Средний рост первого класса = " << sum1 / students << endl;
+    cout << "Средний рост второго класса = " << sum2 / students;
+
+    return 0;
+}
