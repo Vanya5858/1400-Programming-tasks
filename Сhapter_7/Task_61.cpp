0f8fc4bed45bd33378007007f01c4a6eecc765b3
Ivan <iv.vano77@mail.ru>
2025-12-08 13:14:47 +0300

    Add files via upload

---
+++ Сhapter_7/Task_61.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double height;
+    int countLess165 = 0;
+
+    cout << "Введите рост 12 юношей (в см): ";
+    for (int i = 0; i < 12; i++) {
+        cin >> height;
+        if (height < 165)
+            countLess165++;
+    }
+
+    cout << "Количество юношей с ростом менее 165 см: " << countLess165;
+    return 0;
+}
