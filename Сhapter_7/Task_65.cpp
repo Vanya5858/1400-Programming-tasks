45e1f325dc2648282f765d2b52e90f5265856412
Ivan <iv.vano77@mail.ru>
2025-12-09 00:40:20 +0300

    Add files via upload

---
+++ Сhapter_7/Task_65.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int wins, losses, countMoreWins = 0;
+    int teams;
+
+    cout << "Введите количество команд: ";
+    cin >> teams;
+    cout << "Введите количество выигрышей и проигрышей для каждой команды:\n";
+
+    for (int i = 0; i < teams; i++) {
+        cin >> wins >> losses;
+        if (wins > losses)
+            countMoreWins++;
+    }
+
+    cout << "Команд больше выигрышей, чем проигрышей: " << countMoreWins;
+    return 0;
+}
