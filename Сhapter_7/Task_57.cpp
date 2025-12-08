35239f11b40d4975288270b83a5a5ceb25a705bc
Ivan <iv.vano77@mail.ru>
2025-12-08 13:13:51 +0300

    Add files via upload

---
+++ Сhapter_7/Task_57.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double precip, sumEven = 0, sumOdd = 0;
+
+    cout << "Введите количество осадков за каждый день февраля (28 чисел): ";
+
+    for (int day = 1; day <= 28; day++) {
+        cin >> precip;
+        if (day % 2 == 0)
+            sumEven += precip;
+        else
+            sumOdd += precip;
+    }
+
+    cout << (sumEven > sumOdd ? "По чётным числам выпало больше" :
+        sumEven < sumOdd ? "По нечётным числам выпало больше" :
+        "Выпало одинаково");
+    return 0;
+}
