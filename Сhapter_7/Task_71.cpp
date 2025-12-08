f1e10632edcf54904c6bc142e2667da2c7ad243e
Ivan <iv.vano77@mail.ru>
2025-12-09 00:41:27 +0300

    Add files via upload

---
+++ Сhapter_7/Task_71.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int students, grade1, grade2, count2 = 0;
+
+    cout << "Введите количество студентов в группе: ";
+    cin >> students;
+    cout << "Введите оценки каждого студента по двум экзаменам:\n";
+
+    for (int i = 0; i < students; i++) {
+        cin >> grade1 >> grade2;
+        if (grade1 == 2 || grade2 == 2)
+            count2++;
+    }
+
+    cout << "Количество студентов, получивших двойку: " << count2;
+    return 0;
+}
