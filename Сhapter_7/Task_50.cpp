043cdac90a85b46f6f5087cb17a42aca04121e92
Ivan <iv.vano77@mail.ru>
2025-12-08 13:12:00 +0300

    Add files via upload

---
+++ Сhapter_7/Task_50.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int students, total = 0;
+
+    cout << "Введите количество учеников в классах с 1 по 11 (11 чисел): ";
+
+    for (int grade = 1; grade <= 11; grade++) {
+        cin >> students;
+        if (grade % 2 == 1)
+            total += students;
+    }
+
+    cout << "Общее число учеников в нечётных классах = " << total;
+    return 0;
+}
