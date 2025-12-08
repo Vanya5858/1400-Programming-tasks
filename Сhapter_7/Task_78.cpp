b258e2d57cdfa1a31d29a5a3244c1868da8f9fa8
Ivan <iv.vano77@mail.ru>
2025-12-09 00:42:47 +0300

    Add files via upload

---
+++ Сhapter_7/Task_78.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int grade;
+    int count5 = 0, count4 = 0, count3 = 0, count2 = 0;
+    int students;
+
+    cout << "Введите количество учеников в классе: ";
+    cin >> students;
+    cout << "Введите оценки по физике для каждого ученика: ";
+
+    for (int i = 0; i < students; i++) {
+        cin >> grade;
+        switch (grade) {
+        case 5: count5++; break;
+        case 4: count4++; break;
+        case 3: count3++; break;
+        case 2: count2++; break;
+        }
+    }
+
+    cout << "\nРезультаты:\n";
+    cout << "Пятёрок: " << count5 << endl;
+    cout << "Четвёрок: " << count4 << endl;
+    cout << "Троек: " << count3 << endl;
+    cout << "Двоек: " << count2;
+
+    return 0;
+}
