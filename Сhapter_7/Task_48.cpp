f240cc1a77bf7eddfa3301d441d793f2e4e10ea0
Ivan <iv.vano77@mail.ru>
2025-12-08 13:11:37 +0300

    Add files via upload

---
+++ Сhapter_7/Task_48.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int pages, totalJournal = 0;
+    bool readingJournals = false;
+
+    cout << "Введите количество страниц в газетах и журналах (окончание ввода - 0): ";
+
+    while (true) {
+        cin >> pages;
+        if (pages == 0) break;
+
+        if (pages > 16) {
+            totalJournal += pages;
+            readingJournals = true;
+        }
+        else if (readingJournals) {
+            break;
+        }
+    }
+
+    cout << "Общее число страниц во всех журналах = " << totalJournal;
+    return 0;
+}
