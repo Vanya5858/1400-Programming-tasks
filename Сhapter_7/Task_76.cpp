8132c169f857c4f7647ea3141fca1474507fe6de
Ivan <iv.vano77@mail.ru>
2025-12-09 00:42:22 +0300

    Add files via upload

---
+++ Сhapter_7/Task_76.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int team, duration;
+    int removals1 = 0, removals2 = 0;
+    int time1 = 0, time2 = 0;
+
+    cout << "Введите данные по 24 удалениям (номер команды и время: 2, 5 или 10):\n";
+
+    for (int i = 0; i < 24; i++) {
+        cin >> team >> duration;
+
+        if (team == 1) {
+            removals1++;
+            time1 += duration;
+        }
+        else if (team == 2) {
+            removals2++;
+            time2 += duration;
+        }
+    }
+
+    cout << "\nКоманда 1:\n";
+    cout << "  Количество удалений: " << removals1 << endl;
+    cout << "  Общее время: " << time1 << " мин\n";
+
+    cout << "\nКоманда 2:" << endl;
+    cout << "  Количество удалений: " << removals2 << endl;
+    cout << "  Общее время: " << time2 << " мин\n";
+
+    return 0;
+}
