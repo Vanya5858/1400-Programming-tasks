1f5b800fa43754ba76fe72e663d9b051e6eaf7d9
Ivan <iv.vano77@mail.ru>
2025-12-08 13:13:13 +0300

    Add files via upload

---
+++ Сhapter_7/Task_54.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    double d, sumGreater20_5 = 0, p;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    cout << "Введите " << n << " вещественных чисел: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> d;
+        if (d > 20.5)
+            sumGreater20_5 += d;
+    }
+
+    cout << "Введите значение p: ";
+    cin >> p;
+
+    cout << (sumGreater20_5 < p ? "Верно: сумма < p" : "Неверно: сумма >= p");
+    return 0;
+}
