3984f8afa0047ea94cdf2db7125fdb0047b5ccff
Ivan <iv.vano77@mail.ru>
2025-12-08 13:13:27 +0300

    Add files via upload

---
+++ Сhapter_7/Task_55.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n, a, m, q, sumNotGreaterM = 0;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    cout << "Введите значение m: ";
+    cin >> m;
+    cout << "Введите " << n << " целых чисел: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (a <= m)
+            sumNotGreaterM += a;
+    }
+
+    cout << "Введите значение q: ";
+    cin >> q;
+
+    cout << (sumNotGreaterM > q ? "Верно: сумма > q" : "Неверно: сумма <= q");
+    return 0;
+}
