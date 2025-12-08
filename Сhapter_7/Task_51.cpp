b4713518ace8187e52bc08e3d1b7851d38782110
Ivan <iv.vano77@mail.ru>
2025-12-08 13:12:12 +0300

    Add files via upload

---
+++ Сhapter_7/Task_51.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, sum = 0;
+
+    cout << "Введите последовательность целых чисел (первое число нечётное): ";
+
+    cin >> num;
+
+    while (num % 2 != 0) {
+        sum += num;
+        cin >> num;
+    }
+
+    cout << "Сумма идущих подряд в начале нечётных чисел = " << sum;
+    return 0;
+}
