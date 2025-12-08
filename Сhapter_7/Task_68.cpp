e08debbd66c747f5fca7018794bb56eeb58372c1
Ivan <iv.vano77@mail.ru>
2025-12-09 00:40:55 +0300

    Add files via upload

---
+++ Сhapter_7/Task_68.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n, num, firstNum, countEqual = 1;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    if (n <= 0) return 0;
+
+    cout << "Введите " << n << " целых чисел: ";
+    cin >> firstNum;
+
+    for (int i = 1; i < n; i++) {
+        cin >> num;
+        if (num == firstNum)
+            countEqual++;
+        else
+            break;
+    }
+
+    cout << "Количество равных первых элементов: " << countEqual;
+    return 0;
+}
