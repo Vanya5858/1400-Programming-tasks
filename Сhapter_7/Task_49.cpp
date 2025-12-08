81dc82df1a1336176d9bf4b885174fcf619d6e93
Ivan <iv.vano77@mail.ru>
2025-12-08 13:11:49 +0300

    Add files via upload

---
+++ Сhapter_7/Task_49.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double precip, totalEven = 0;
+    int daysInMonth;
+
+    cout << "Введите количество дней в месяце: ";
+    cin >> daysInMonth;
+    cout << "Введите количество осадков за каждый день: ";
+
+    for (int day = 1; day <= daysInMonth; day++) {
+        cin >> precip;
+        if (day % 2 == 0)
+            totalEven += precip;
+    }
+
+    cout << "Общее количество осадков в чётные числа = " << totalEven;
+    return 0;
+}
