60672361eb5aec57a0971268d69e1abdabfd67f3
Ivan <iv.vano77@mail.ru>
2025-12-09 00:40:44 +0300

    Add files via upload

---
+++ Сhapter_7/Task_67.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double num;
+    int countBeforeZero = 0;
+
+    cout << "Введите последовательность вещественных чисел (завершается 0): ";
+
+    while (true) {
+        cin >> num;
+        if (num == 0)
+            break;
+        countBeforeZero++;
+    }
+
+    cout << "Количество чисел перед первым нулём: " << countBeforeZero;
+    return 0;
+}
