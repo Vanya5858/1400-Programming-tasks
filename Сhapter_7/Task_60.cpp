e5367511d54c3b6a0d924063ac48d1c93a792c95
Ivan <iv.vano77@mail.ru>
2025-12-08 13:14:23 +0300

    Add files via upload

---
+++ Сhapter_7/Task_60.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double temperature;
+    int daysBelow0 = 0;
+    int daysInMonth;
+
+    cout << "Введите количество дней в месяце: ";
+    cin >> daysInMonth;
+    cout << "Введите температуру за каждый день: ";
+
+    for (int day = 1; day <= daysInMonth; day++) {
+        cin >> temperature;
+        if (temperature < 0)
+            daysBelow0++;
+    }
+
+    cout << "Температура опускалась ниже 0°С в " << daysBelow0 << " днях";
+    return 0;
+}
