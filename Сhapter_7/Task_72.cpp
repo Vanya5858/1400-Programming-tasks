78b84c7b5987591a9659a2ee0c254eda6b37cf22
Ivan <iv.vano77@mail.ru>
2025-12-09 00:41:37 +0300

    Add files via upload

---
+++ Сhapter_7/Task_72.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    double a;
+    int countNeg = 0, countPos = 0;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    cout << "Введите " << n << " вещественных чисел: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (a < 0)
+            countNeg++;
+        else if (a > 0)
+            countPos++;
+    }
+
+    cout << "Количество отрицательных: " << countNeg << endl;
+    cout << "Количество положительных: " << countPos;
+    return 0;
+}
