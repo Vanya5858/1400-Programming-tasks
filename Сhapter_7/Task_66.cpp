c0b6b0302c4432734d8b33c4ae4dee08c7c39f02
Ivan <iv.vano77@mail.ru>
2025-12-09 00:40:33 +0300

    Add files via upload

---
+++ Сhapter_7/Task_66.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double num;
+    int countNegative = 0;
+    int n;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    cout << "Введите последовательность вещественных чисел (начинается с отрицательного): ";
+
+    cin >> num;
+
+    while (num < 0 && countNegative < n) {
+        countNegative++;
+        if (countNegative < n)
+            cin >> num;
+    }
+
+    cout << "Количество отрицательных чисел в начале: " << countNegative;
+    return 0;
+}
