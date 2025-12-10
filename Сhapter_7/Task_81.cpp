028c84efb7d56dbb6018ea1dba5f03e2d9e53ef7
Ivan <iv.vano77@mail.ru>
2025-12-10 21:57:30 +0300

    Add files via upload

---
+++ Сhapter_7/Task_81.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, twoDigitCount = 0;
+
+    cout << "Введите последовательность чисел (двузначное число кодирует забитые и пропущенные):\n";
+
+    for (int i = 0; i < 20; i++) {
+        cin >> num;
+
+        // Разделяем двузначное число на цифры
+        int scored = num / 10;
+        int conceded = num % 10;
+
+        if (scored > conceded)
+            cout << "выигрыш\n";
+        else if (scored < conceded)
+            cout << "проигрыш\n";
+        else
+            cout << "ничья\n";
+    }
+
+    return 0;
+}
