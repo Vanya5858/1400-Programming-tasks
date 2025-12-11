d2aff246f506909a1fa57a5f34554efa68bfd9d5
Ivan <iv.vano77@mail.ru>
2025-12-11 22:10:53 +0300

    Add files via upload

---
+++ Сhapter_7/Task_89.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int m, a, countGreaterM = 0, p;
+
+    cout << "Введите количество чисел m: ";
+    cin >> m;
+    cout << "Введите число для сравнения: ";
+    cin >> p;
+    cout << "Введите " << m << " целых чисел: ";
+
+    for (int i = 0; i < m; i++) {
+        cin >> a;
+        if (a > m)
+            countGreaterM++;
+    }
+
+    cout << (countGreaterM % p == 0 ?
+        "Верно: количество чисел > m кратно p" :
+        "Неверно: количество чисел > m не кратно p");
+    return 0;
+}
