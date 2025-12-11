39bae52f940f3ca244f67771ea3464e222532174
Ivan <iv.vano77@mail.ru>
2025-12-11 22:07:27 +0300

    Add files via upload

---
+++ Сhapter_7/Task_83.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n, num;
+    bool hasEven = false;
+
+    cout << "Введите количество чисел в наборе: ";
+    cin >> n;
+    cout << "Введите " << n << " целых чисел: ";
+
+    for (int i = 0; i < n && !hasEven; i++) {
+        cin >> num;
+        if (num % 2 == 0)
+            hasEven = true;
+    }
+
+    // Досчитываем остальные числа, если нужно
+    for (int i = (hasEven ? n : 0); i < n; i++) {
+        cin >> num;
+    }
+
+    cout << (hasEven ? "В наборе есть чётное число" : "В наборе нет чётных чисел");
+    return 0;
+}
