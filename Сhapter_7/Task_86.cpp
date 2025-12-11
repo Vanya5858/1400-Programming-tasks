95595507d7f9367ba7f2e5c7d4a1de5c58b3d7fc
Ivan <iv.vano77@mail.ru>
2025-12-11 22:09:03 +0300

    Add files via upload

---
+++ Сhapter_7/Task_86.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n, c, countLess20 = 0;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    cout << "Введите " << n << " целых чисел: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> c;
+        if (c < 20)
+            countLess20++;
+    }
+
+    cout << (countLess20 == 5 ? "Верно: ровно 5 чисел меньше 20" :
+        "Неверно: не ровно 5 чисел меньше 20");
+    return 0;
+}
