758f8ce26bcc09df44c92d4fb11c03af617c250d
Ivan <iv.vano77@mail.ru>
2025-12-11 22:07:50 +0300

    Add files via upload

---
+++ Сhapter_7/Task_84.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int a, positiveCount = 0;
+
+    cout << "Введите 10 целых чисел: ";
+    for (int i = 0; i < 10; i++) {
+        cin >> a;
+        if (a > 0)
+            positiveCount++;
+    }
+
+    cout << (positiveCount <= 5 ? "Верно: положительных не более 5" :
+        "Неверно: положительных больше 5");
+    return 0;
+}
