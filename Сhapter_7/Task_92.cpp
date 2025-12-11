aa76d1b5e2319199147ece4abe248a9f0675877d
Ivan <iv.vano77@mail.ru>
2025-12-11 22:17:01 +0300

    Add files via upload

---
+++ Сhapter_7/Task_92.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int grade;
+    bool hasGrade2 = false;
+
+    cout << "Введите оценки по информатике 28 учеников: ";
+
+    for (int i = 0; i < 28 && !hasGrade2; i++) {
+        cin >> grade;
+        if (grade == 2)
+            hasGrade2 = true;
+    }
+
+    // Досчитываем остальные оценки
+    for (int i = (hasGrade2 ? 28 : 0); i < 28; i++) {
+        cin >> grade;
+    }
+
+    cout << (hasGrade2 ? "Есть двойки" : "Нет двоек");
+    return 0;
+}
