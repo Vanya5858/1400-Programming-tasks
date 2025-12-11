9a70ecedd225f825e2cb59d6d11199c9c0f1b789
Ivan <iv.vano77@mail.ru>
2025-12-11 22:13:20 +0300

    Add files via upload

---
+++ Сhapter_7/Task_90.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int grade;
+    bool hasGrade3 = false;
+
+    cout << "Введите оценки ученика по 12 предметам: ";
+
+    for (int i = 0; i < 12; i++) {
+        cin >> grade;
+        if (grade == 3) {
+            hasGrade3 = true;
+        }
+    }
+
+    cout << (hasGrade3 ? "Среди оценок есть тройки" :
+        "Среди оценок нет троек") << endl;
+
+    cout << "Можно ли использовать while? Да, но for удобнее.";
+
+    return 0;
+}
