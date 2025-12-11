51ac095664def25b266a78a8a4d3386cf099f50e
Ivan <iv.vano77@mail.ru>
2025-12-11 22:08:35 +0300

    Add files via upload

---
+++ Сhapter_7/Task_85.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double x;
+    int countNotGreater33_2 = 0;
+
+    cout << "Введите 15 вещественных чисел: ";
+    for (int i = 0; i < 15; i++) {
+        cin >> x;
+        if (x <= 33.2)
+            countNotGreater33_2++;
+    }
+
+    cout << (countNotGreater33_2 % 4 == 0 ?
+        "Верно: количество кратно 4" :
+        "Неверно: количество не кратно 4");
+    return 0;
+}
