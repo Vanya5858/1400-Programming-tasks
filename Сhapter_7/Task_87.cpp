7d5f14ed1af55f180550e72cc3faa4ec2530cf0a
Ivan <iv.vano77@mail.ru>
2025-12-11 22:09:16 +0300

    Add files via upload

---
+++ Сhapter_7/Task_87.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int m, d, positiveCount = 0;
+
+    cout << "Введите количество чисел m: ";
+    cin >> m;
+    cout << "Введите " << m << " целых чисел: ";
+
+    for (int i = 0; i < m; i++) {
+        cin >> d;
+        if (d > 0)
+            positiveCount++;
+    }
+
+    cout << (positiveCount % 3 == 0 ?
+        "Верно: количество положительных кратно 3" :
+        "Неверно: количество положительных не кратно 3");
+    return 0;
+}
