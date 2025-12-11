8081bfc209747c29a413b1c51d4ab84e2fea6e5b
Ivan <iv.vano77@mail.ru>
2025-12-11 22:09:37 +0300

    Add files via upload

---
+++ Сhapter_7/Task_88.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n, a, negativeCount = 0, x;
+
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+    cout << "Введите значение x: ";
+    cin >> x;
+    cout << "Введите " << n << " целых чисел: ";
+
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        if (a < 0)
+            negativeCount++;
+    }
+
+    cout << (negativeCount > x ? "Верно: отрицательных больше x" :
+        "Неверно: отрицательных не больше x");
+    return 0;
+}
