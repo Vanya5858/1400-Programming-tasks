626100d12fd2b455f9690d9dec07517e7e51929c
Ivan <iv.vano77@mail.ru>
2025-12-12 22:45:38 +0300

    Add files via upload

---
+++ Сhapter_7/Task_93.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double power;
+    bool hasPowerOver200 = false;
+
+    cout << "Введите мощность двигателя 30 моделей автомобилей: ";
+
+    for (int i = 0; i < 30 && !hasPowerOver200; i++) {
+        cin >> power;
+        if (power > 200)
+            hasPowerOver200 = true;
+    }
+
+    // Досчитываем остальные
+    for (int i = (hasPowerOver200 ? 30 : 0); i < 30; i++) {
+        cin >> power;
+    }
+
+    cout << (hasPowerOver200 ? "Есть модель мощнее 200 л.с." :
+        "Нет моделей мощнее 200 л.с.");
+    return 0;
+}
