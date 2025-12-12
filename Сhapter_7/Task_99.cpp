12571e602efd434696ffab918706c773a7a5f7d0
Ivan <iv.vano77@mail.ru>
2025-12-12 22:46:43 +0300

    Add files via upload

---
+++ Сhapter_7/Task_99.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, prev;
+    bool foundEvenPair = false;
+    int count = 1;
+    int firstIndex = -1;
+
+    cout << "Введите последовательность целых чисел (оканчивается 9999): ";
+
+    cin >> prev;
+    if (prev == 9999) {
+        cout << "Последовательность слишком короткая";
+        return 0;
+    }
+
+    while (true) {
+        cin >> num;
+        if (num == 9999) break;
+
+        count++;
+
+        if (!foundEvenPair && prev % 2 == 0 && num % 2 == 0) {
+            foundEvenPair = true;
+            firstIndex = count - 1;
+        }
+
+        prev = num;
+    }
+
+    if (foundEvenPair) {
+        cout << "Есть пара соседних чётных чисел\n";
+        cout << "Порядковые номера: " << firstIndex << " и " << firstIndex + 1;
+    }
+    else {
+        cout << "Нет пар соседних чётных чисел";
+    }
+
+    return 0;
+}
