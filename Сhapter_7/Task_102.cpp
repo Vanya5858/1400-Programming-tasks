12f70a715ab523ef1de6c86f3fc3ef5f4972bc98
Ivan <iv.vano77@mail.ru>
2025-12-12 22:47:17 +0300

    Add files via upload

---
+++ Сhapter_7/Task_102.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double height, prev;
+    bool isDecreasing = true;
+    int students;
+
+    cout << "Введите количество учеников в классе: ";
+    cin >> students;
+
+    if (students < 2) {
+        cout << "Нужно минимум 2 ученика";
+        return 0;
+    }
+
+    cout << "Введите рост учеников в порядке списка: ";
+    cin >> prev;
+
+    for (int i = 1; i < students; i++) {
+        cin >> height;
+
+        if (height > prev) {
+            isDecreasing = false;
+        }
+
+        prev = height;
+    }
+
+    cout << (isDecreasing ? "Ученики перечислены в порядке убывания роста" :
+        "Ученики не перечислены в порядке убывания роста");
+    return 0;
+}
