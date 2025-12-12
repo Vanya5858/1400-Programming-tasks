4963806815b95685afdd6ea75b8e1d029b4ade58
Ivan <iv.vano77@mail.ru>
2025-12-12 22:46:22 +0300

    Add files via upload

---
+++ Сhapter_7/Task_97.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, prev;
+    bool foundPair = false;
+    int count = 1;
+    int firstIndex = -1;
+
+    cout << "Введите последовательность целых чисел (оканчивается -1): ";
+
+    cin >> prev;
+    if (prev == -1) {
+        cout << "Последовательность слишком короткая";
+        return 0;
+    }
+
+    while (true) {
+        cin >> num;
+        if (num == -1) break;
+
+        count++;
+
+        if (!foundPair && num == prev) {
+            foundPair = true;
+            firstIndex = count - 1; 
+        }
+
+        prev = num;
+    }
+
+    if (foundPair) {
+        cout << "Есть пара одинаковых соседних чисел\n";
+        cout << "Порядковые номера: " << firstIndex << " и " << firstIndex + 1;
+    }
+    else {
+        cout << "Нет пар одинаковых соседних чисел";
+    }
+
+    return 0;
+}
