71ff9ebb6ec3e162fb8ac7ae8bc930a2e1956e02
Ivan <iv.vano77@mail.ru>
2025-12-12 22:46:57 +0300

    Add files via upload

---
+++ Сhapter_7/Task_100.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double x[15];
+    bool isOrdered = true;
+    int firstWrongIndex = -1;
+
+    cout << "Введите 15 вещественных чисел: ";
+
+    cin >> x[0];
+
+    for (int i = 1; i < 15; i++) {
+        cin >> x[i];
+
+        if (isOrdered && x[i] < x[i - 1]) {
+            isOrdered = false;
+            firstWrongIndex = i + 1;
+        }
+    }
+
+    if (isOrdered) {
+        cout << "Последовательность упорядочена по возрастанию";
+    }
+    else {
+        cout << "Последовательность не упорядочена по возрастанию\n";
+        cout << "Порядковый номер первого числа, нарушающего упорядоченность: "
+            << firstWrongIndex;
+    }
+
+    return 0;
+}
