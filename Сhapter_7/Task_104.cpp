e776025076399e1710ef2c563c665039db42c591
Ivan <iv.vano77@mail.ru>
2025-12-13 08:17:45 +0300

    Add files via upload

---
+++ Сhapter_7/Task_104.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int x, first;
+    bool allEqual = true;
+
+    cout << "Введите 12 целых чисел: ";
+
+    cin >> first;
+
+    for (int i = 1; i < 12; i++) {
+        cin >> x;
+        if (x != first) {
+            allEqual = false;
+        }
+    }
+
+    cout << (allEqual ? "Все элементы равны" : "Не все элементы равны");
+    return 0;
+}
