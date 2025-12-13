74b8b992e63381d71d3e24457b0c6e3474fad1d7
Ivan <iv.vano77@mail.ru>
2025-12-13 08:17:32 +0300

    Add files via upload

---
+++ Сhapter_7/Task_103.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int points, prev;
+    bool isDecreasing = true;
+    int teams;
+
+    cout << "Введите количество футбольных команд: ";
+    cin >> teams;
+
+    if (teams < 2) {
+        cout << "Нужно минимум 2 команды";
+        return 0;
+    }
+
+    cout << "Введите сумму очков каждой команды: ";
+    cin >> prev;
+
+    for (int i = 1; i < teams; i++) {
+        cin >> points;
+
+        if (points > prev) {
+            isDecreasing = false;
+        }
+
+        prev = points;
+    }
+
+    cout << (isDecreasing ? "Команды перечислены в соответствии с занятыми местами" :
+        "Команды не перечислены в соответствии с занятыми местами");
+    return 0;
+}
