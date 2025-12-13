4e421c66035fbd88c6c27b58d809d87909899d39
Ivan <iv.vano77@mail.ru>
2025-12-13 08:17:58 +0300

    Add files via upload

---
+++ Сhapter_7/Task_105.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, first;
+    bool allEqual = true;
+    bool firstRead = false;
+
+    cout << "Введите последовательность целых чисел (оканчивается отрицательным): ";
+
+    while (true) {
+        cin >> num;
+        if (num < 0) break;
+
+        if (!firstRead) {
+            first = num;
+            firstRead = true;
+        }
+        else if (num != first) {
+            allEqual = false;
+        }
+    }
+
+    if (!firstRead) {
+        cout << "Последовательность пуста";
+    }
+    else {
+        cout << (allEqual ? "Все элементы равны" : "Не все элементы равны");
+    }
+
+    return 0;
+}
