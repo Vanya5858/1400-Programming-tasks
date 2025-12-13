287dea4d1bfd5713c1dbd4f95f1dea523cd3e2a3
Ivan <iv.vano77@mail.ru>
2025-12-13 08:18:22 +0300

    Add files via upload

---
+++ Сhapter_7/Task_107.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int x, a, n;
+    int sumGreaterN = 0, countGreaterN = 0;
+
+    cout << "Введите количество чисел x: ";
+    cin >> x;
+    cout << "Введите значение n: ";
+    cin >> n;
+    cout << "Введите " << x << " целых чисел: ";
+
+    for (int i = 0; i < x; i++) {
+        cin >> a;
+        if (a > n) {
+            sumGreaterN += a;
+            countGreaterN++;
+        }
+    }
+
+    if (countGreaterN > 0) {
+        double average = (double)sumGreaterN / countGreaterN;
+        cout << "Среднее арифметическое чисел > " << n << ": " << average;
+    }
+    else {
+        cout << "Чисел > " << n << " не найдено (хотя должны быть)";
+    }
+
+    return 0;
+}
