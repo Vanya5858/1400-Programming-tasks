4794b707aa00bac85e3ab8310c628ea4bb83be55
Ivan <iv.vano77@mail.ru>
2025-12-13 08:18:34 +0300

    Add files via upload

---
+++ Сhapter_7/Task_108.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int m, b, n;
+    int sumMultipleN = 0, countMultipleN = 0;
+
+    cout << "Введите количество чисел m: ";
+    cin >> m;
+    cout << "Введите значение n: ";
+    cin >> n;
+    cout << "Введите " << m << " целых чисел: ";
+
+    for (int i = 0; i < m; i++) {
+        cin >> b;
+        if (b % n == 0) {
+            sumMultipleN += b;
+            countMultipleN++;
+        }
+    }
+
+    if (countMultipleN > 0) {
+        double average = (double)sumMultipleN / countMultipleN;
+        cout << "Среднее арифметическое чисел, кратных " << n << ": " << average;
+    }
+    else {
+        cout << "Чисел, кратных " << n << ", не найдено (хотя должны быть)";
+    }
+
+    return 0;
+}
