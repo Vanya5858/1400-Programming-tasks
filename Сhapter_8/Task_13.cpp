0c39fd5b756aca2adb6c08a2b4add4eea0b471a1
Ivan <iv.vano77@mail.ru>
2025-12-15 01:06:56 +0300

    Add files via upload

---
+++ Сhapter_8/Task_13.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a;
+    cout << "Введите a: ";
+    cin >> a;
+
+    double sum = 0;
+    int n = 1;
+
+    while (sum <= a) {
+        sum += 1.0 / n;
+        n++;
+    }
+
+    cout << "Наименьшее n: " << n - 1 << endl;
+    return 0;
+}
