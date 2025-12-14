9bbbd8e631adbd1e5a50e93bb64330493ef8fafe
Ivan <iv.vano77@mail.ru>
2025-12-15 01:06:24 +0300

    Add files via upload

---
+++ Сhapter_8/Task_11.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double limit;
+    cout << "Введите число n: ";
+    cin >> limit;
+
+    double sum = 0;
+    int n = 1;
+    while (sum <= limit) {
+        sum += 1.0 / n;
+        n++;
+    }
+    cout << "Первая сумма > n: " << sum << " при n = " << n - 1 << endl;
+    return 0;
+}
