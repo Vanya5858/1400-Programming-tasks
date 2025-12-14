595dee030c73c624613b249f524d65ddb81764ac
Ivan <iv.vano77@mail.ru>
2025-12-15 01:06:39 +0300

    Add files via upload

---
+++ Сhapter_8/Task_12.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a;
+    cout << "Введите a: ";
+    cin >> a;
+
+    double sum = 0;
+    int n = 1;
+
+    while (true) {
+        sum += 1.0 / n;
+        if (sum > a) {
+            cout << n << " ";
+        }
+        n++;
+        if (n > 1000) break; // ограничим для безопасности
+    }
+
+    return 0;
+}
