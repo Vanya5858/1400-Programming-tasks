632623c3cd268c98fed0418f14fb2cf14a1babb8
Ivan <iv.vano77@mail.ru>
2025-12-14 23:50:45 +0300

    Add files via upload

---
+++ Сhapter_8/Task_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите n: ";
+    cin >> n;
+
+    // Способ 1: цикл while
+    int i = 1;
+    while (i * i <= n) i++;
+    cout << "Первое число > n: " << i * i << endl;
+
+    // Способ 2: for
+    for (int j = 1; ; j++) {
+        if (j * j > n) {
+            cout << "Первое число > n: " << j * j << endl;
+            break;
+        }
+    }
+    return 0;
+}
