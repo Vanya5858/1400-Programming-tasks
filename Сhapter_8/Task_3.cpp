8c1eb76683a3a6ca4cf986f846721802c42574ea
Ivan <iv.vano77@mail.ru>
2025-12-14 23:50:58 +0300

    Add files via upload

---
+++ Сhapter_8/Task_3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double a;
+    cout << "Введите a (0 < a <= 1): ";
+    cin >> a;
+
+    int znam = 1;
+    while (1.0 / znam >= a) {
+        cout << "1/" << znam << " = " << 1.0 / znam << endl;
+        znam++;
+    }
+    return 0;
+}
