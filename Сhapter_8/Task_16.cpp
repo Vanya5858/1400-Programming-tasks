d321391b7f9ae40ec06fffa2f517025810d11ad5
Ivan <iv.vano77@mail.ru>
2025-12-15 01:07:32 +0300

    Add files via upload

---
+++ Сhapter_8/Task_16.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double m;
+    cout << "Введите m (0.5 < m < 1): ";
+    cin >> m;
+
+    // Вариант 1: цикл while
+    int i = 1;
+    while (i / (i + 1.0) < m) {
+        cout << i / (i + 1.0) << " ";
+        i++;
+    }
+    cout << endl;
+
+    // Вариант 2: цикл for
+    for (int j = 1; j / (j + 1.0) < m; j++) {
+        cout << j / (j + 1.0) << " ";
+    }
+
+    return 0;
+}
