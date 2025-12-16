b54d5ce5febb6d8f86ba8e296efce657e4335adb
Ivan <iv.vano77@mail.ru>
2025-12-16 23:53:48 +0300

    Add files via upload

---
+++ Сhapter_7/Task_121.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int k;
+    cout << "Введите количество чисел k: ";
+    cin >> k;
+
+    int lastNegIndex = -1;
+
+    cout << "Введите " << k << " целых чисел: ";
+    for (int i = 1; i <= k; i++) {
+        int num;
+        cin >> num;
+        if (num < 0) {
+            lastNegIndex = i;
+        }
+    }
+
+    // По условию известно, что отрицательные числа есть
+    cout << "Номер последнего отрицательного числа: " << lastNegIndex << endl;
+
+    return 0;
+}
