7603f05154cbc7dd460f6412f64bea2dfbff8ec1
Ivan <iv.vano77@mail.ru>
2025-12-16 23:53:04 +0300

    Add files via upload

---
+++ Сhapter_7/Task_118.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+
+    int firstIndex = -1, lastIndex = -1;
+
+    cout << "Введите " << n << " целых чисел: ";
+    for (int i = 1; i <= n; i++) {
+        int num;
+        cin >> num;
+        if (num == 10) {
+            if (firstIndex == -1) firstIndex = i;
+            lastIndex = i;
+        }
+    }
+
+    if (firstIndex != -1) {
+        cout << "Номер первого числа 10: " << firstIndex << endl;
+        cout << "Номер последнего числа 10: " << lastIndex << endl;
+    }
+    else {
+        cout << "Числа 10 не найдены\n";
+    }
+
+    return 0;
+}
