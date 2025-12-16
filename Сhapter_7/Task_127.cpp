54fcc7d049e1434416f26fb5dd6bf05df875227e
Ivan <iv.vano77@mail.ru>
2025-12-17 00:45:08 +0300

    Add files via upload

---
+++ Сhapter_7/Task_127.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, count = 0, firstIndex = -1;
+
+    cout << "Введите последовательность целых чисел, оканчивающуюся 100: ";
+    while (true) {
+        cin >> num;
+        count++;
+        if (num == 100) break;
+        if (num == 666 && firstIndex == -1) {
+            firstIndex = count;
+        }
+    }
+
+    if (firstIndex != -1) {
+        cout << "Номер первого числа 666: " << firstIndex << endl;
+    }
+    else {
+        cout << "Числа 666 нет в последовательности\n";
+    }
+
+    return 0;
+}
