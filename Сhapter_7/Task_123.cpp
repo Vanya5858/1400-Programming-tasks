ab010fae953b6cc312470b10ad8ebd3eafd0918c
Ivan <iv.vano77@mail.ru>
2025-12-17 00:44:22 +0300

    Add files via upload

---
+++ Сhapter_7/Task_123.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const int N = 15;
+    int heights[N];
+
+    cout << "Введите рост 15 учеников (по убыванию): ";
+    for (int i = 0; i < N; i++) {
+        cin >> heights[i];
+    }
+
+    int newHeight;
+    cout << "Введите рост нового ученика: ";
+    cin >> newHeight;
+
+    int place = 1;
+    while (place <= N && newHeight < heights[place - 1]) {
+        place++;
+    }
+
+    cout << "Место нового ученика: " << place << endl;
+    return 0;
+}
