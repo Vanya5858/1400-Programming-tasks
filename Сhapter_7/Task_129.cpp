5c263699f0e99ad91d912f725a2b16116e518136
Ivan <iv.vano77@mail.ru>
2025-12-17 00:45:32 +0300

    Add files via upload

---
+++ Сhapter_7/Task_129.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, count = 0, firstIndex = -1;
+
+    cout << "Введите последовательность целых чисел, оканчивающуюся -1: ";
+    while (true) {
+        cin >> num;
+        count++;
+        if (num == -1) break;
+        if (num % 7 == 0 && firstIndex == -1) {
+            firstIndex = count;
+        }
+    }
+
+    if (firstIndex != -1) {
+        cout << "Номер первого числа, кратного 7: " << firstIndex << endl;
+    }
+    else {
+        cout << "Чисел, кратных 7, нет\n";
+    }
+
+    return 0;
+}
