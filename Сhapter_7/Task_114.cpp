8d2e084b3923feb106901ce02d5003187ef1e2e4
Ivan <iv.vano77@mail.ru>
2025-12-16 23:52:16 +0300

    Add files via upload

---
+++ Сhapter_7/Task_114.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    cout << "Введите 14 целых чисел: ";
+    int count = 0, sum = 0;
+
+    for (int i = 0; i < 14; i++) {
+        int num;
+        cin >> num;
+        if (num % 2 == 0) {
+            sum += num;
+            count++;
+        }
+    }
+
+    if (count > 0) {
+        cout << "Среднее арифметическое четных чисел: " << (double)sum / count << endl;
+    }
+    else {
+        cout << "Нет четных чисел\n";
+    }
+
+    return 0;
+}
