bfbeb19bae75e48795436865f4de45bd787185d0
Ivan <iv.vano77@mail.ru>
2025-12-17 00:45:21 +0300

    Add files via upload

---
+++ Сhapter_7/Task_128.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const int SIZE = 12;
+    int b[SIZE];
+
+    cout << "Введите 12 натуральных чисел: ";
+    for (int i = 0; i < SIZE; i++) {
+        cin >> b[i];
+    }
+
+    int firstIndex = -1;
+    for (int i = 0; i < SIZE; i++) {
+        if (b[i] % 10 == 7) {
+            firstIndex = i + 1;
+            break;
+        }
+    }
+
+    if (firstIndex != -1) {
+        cout << "Номер первого числа, оканчивающегося на 7: " << firstIndex << endl;
+    }
+    else {
+        cout << "Таких чисел нет\n";
+    }
+
+    return 0;
+}
