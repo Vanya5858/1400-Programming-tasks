a9bf986b142b04b1fa4824ddfc6b9a8fb8c072cf
Ivan <iv.vano77@mail.ru>
2025-12-17 00:44:08 +0300

    Add files via upload

---
+++ Сhapter_7/Task_122.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int m;
+    cout << "Введите количество чисел m: ";
+    cin >> m;
+
+    int lastIndex = -1;
+
+    cout << "Введите " << m << " целых чисел: ";
+    for (int i = 1; i <= m; i++) {
+        int num;
+        cin >> num;
+        if (abs(num) % 100 == 12) { // оканчивается на 12
+            lastIndex = i;
+        }
+    }
+
+    if (lastIndex != -1) {
+        cout << "Номер последнего числа, оканчивающегося на 12: " << lastIndex << endl;
+    }
+    else {
+        cout << "Таких чисел нет\n";
+    }
+
+    return 0;
+}
