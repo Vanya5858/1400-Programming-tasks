70af0630817fbbaa1ee84c9ee222b54f6db039bc
Ivan <iv.vano77@mail.ru>
2025-12-16 23:52:03 +0300

    Add files via upload

---
+++ Сhapter_7/Task_113.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int x, n;
+    cout << "Введите количество чисел x: ";
+    cin >> x;
+    cout << "Введите число n: ";
+    cin >> n;
+
+    int count = 0;
+    int sum = 0;
+
+    cout << "Введите " << x << " целых чисел: ";
+    for (int i = 0; i < x; i++) {
+        int num;
+        cin >> num;
+        if (num > n) {
+            sum += num;
+            count++;
+        }
+    }
+
+    if (count > 0) {
+        cout << "Среднее арифметическое чисел > " << n << ": " << (double)sum / count << endl;
+    }
+    else {
+        cout << "Нет чисел > " << n << endl;
+    }
+
+    return 0;
+}
