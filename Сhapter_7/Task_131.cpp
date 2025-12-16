b7395a859bc920944f99854e1b487c600de8af2d
Ivan <iv.vano77@mail.ru>
2025-12-17 00:45:54 +0300

    Add files via upload

---
+++ Сhapter_7/Task_131.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const int SIZE = 20;
+    int arr[SIZE];
+
+    cout << "Введите 20 чисел (неубывающая последовательность): ";
+    for (int i = 0; i < SIZE; i++) {
+        cin >> arr[i];
+    }
+
+    int maxCount = 1, currentCount = 1;
+    for (int i = 1; i < SIZE; i++) {
+        if (arr[i] == arr[i - 1]) {
+            currentCount++;
+            if (currentCount > maxCount) {
+                maxCount = currentCount;
+            }
+        }
+        else {
+            currentCount = 1;
+        }
+    }
+
+    cout << "Максимальное количество подряд идущих равных чисел: " << maxCount << endl;
+    return 0;
+}
