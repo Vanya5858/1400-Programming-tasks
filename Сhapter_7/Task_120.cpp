d27ae8bdd56aafa4515eaaeacc2d0706cfd76179
Ivan <iv.vano77@mail.ru>
2025-12-16 23:53:29 +0300

    Add files via upload

---
+++ Сhapter_7/Task_120.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+
+    int lastIndex = -1;
+
+    cout << "Введите " << n << " целых чисел: ";
+    for (int i = 1; i <= n; i++) {
+        int num;
+        cin >> num;
+        if (num == 25) {
+            lastIndex = i;
+        }
+    }
+
+    if (lastIndex != -1) {
+        cout << "Номер последнего числа = 25: " << lastIndex << endl;
+    }
+    else {
+        cout << "Числа 25 не найдены\n";
+    }
+
+    return 0;
+}
