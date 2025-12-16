7147fc56c292817864c6cd02183c9c665ffffad3
Ivan <iv.vano77@mail.ru>
2025-12-17 00:44:33 +0300

    Add files via upload

---
+++ Сhapter_7/Task_124.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const int N_TEAMS = 20;
+    int points[N_TEAMS];
+
+    cout << "Введите очки 20 команд (по убыванию): ";
+    for (int i = 0; i < N_TEAMS; i++) {
+        cin >> points[i];
+    }
+    
+    int target;
+    cout << "Введите количество очков команды N: ";
+    cin >> target;
+
+    int place = 1;
+    while (place <= N_TEAMS && points[place - 1] > target) {
+        place++;
+    }
+
+    cout << "Команда заняла " << place << " место\n";
+    return 0;
+}
