8ea8af0c866ebd96da0944c172bc464c5ab3dd05
Ivan <iv.vano77@mail.ru>
2025-12-19 23:23:56 +0300

    Add files via upload

---
+++ Сhapter_7/Task_141.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество квадратов: ";
+    cin >> n;
+
+    double maxDiag = 0, area, side, diag;
+
+    for (int i = 0; i < n; i++) {
+        cin >> area;
+        side = sqrt(area);
+        diag = side * sqrt(2);
+        if (diag > maxDiag) {
+            maxDiag = diag;
+        }
+    }
+
+    cout << "Длина диагонали самого большого квадрата: " << maxDiag << endl;
+    return 0;
+}
