ff9e017569afd613fa73d3ce1b71e9f8628c5cbc
Ivan <iv.vano77@mail.ru>
2025-12-19 23:21:20 +0300

    Add files via upload

---
+++ Сhapter_7/Task_133.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, prev, count = 0, totalEqual = 0;
+    bool first = true;
+
+    cout << "Введите последовательность вещественных чисел, оканчивающуюся 1000: ";
+    while (true) {
+        cin >> num;
+        if (num == 1000) break;
+
+        if (first) {
+            prev = num;
+            first = false;
+        }
+        else {
+            if (num == prev) {
+                count++;
+            }
+            else {
+                if (count > 0) totalEqual += count + 1;
+                count = 0;
+                prev = num;
+            }
+        }
+    }
+    // Учитываем последнюю группу
+    if (count > 0) totalEqual += count + 1;
+
+    cout << "Количество подряд идущих равных чисел: " << totalEqual << endl;
+    return 0;
+}
