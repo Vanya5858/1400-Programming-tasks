6ba7bbc616d70569171b0911d15442dd3a652c8a
Ivan <iv.vano77@mail.ru>
2025-12-19 23:23:37 +0300

    Add files via upload

---
+++ Сhapter_7/Task_140.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    const double M_PI = 3.1415926535;
+    cout << "Введите количество кругов: ";
+    cin >> n;
+
+    double minRadius = 1e9, area, radius;
+
+    for (int i = 0; i < n; i++) {
+        cin >> area;
+        radius = sqrt(area / M_PI);
+        if (radius < minRadius) {
+            minRadius = radius;
+        }
+    }
+
+    cout << "Радиус самого маленького круга: " << minRadius << endl;
+    return 0;
+}
