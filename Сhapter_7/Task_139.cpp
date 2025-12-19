45607b5f68903d63d0e4a7282de0a4d3758523e3
Ivan <iv.vano77@mail.ru>
2025-12-19 23:23:22 +0300

    Add files via upload

---
+++ Сhapter_7/Task_139.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const int N = 20;
+    double speeds[N];
+
+    cout << "Введите максимальные скорости 20 марок автомобилей: ";
+    for (int i = 0; i < N; i++) {
+        cin >> speeds[i];
+    }
+
+    double maxSpeed = speeds[0];
+    for (int i = 1; i < N; i++) {
+        if (speeds[i] > maxSpeed) {
+            maxSpeed = speeds[i];
+        }
+    }
+
+    cout << "Максимальная скорость: " << maxSpeed << " км/ч\n";
+    return 0;
+}
