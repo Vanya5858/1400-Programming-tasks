491b2d3d84d06a4efa6f377670f5ee44bc5fb7a2
Ivan <iv.vano77@mail.ru>
2025-12-19 23:22:26 +0300

    Add files via upload

---
+++ Сhapter_7/Task_136.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    double time, bestTime;
+    int count = 0;
+
+    cout << "Вводите время спортсменов (0 для окончания): ";
+    while (true) {
+        cin >> time;
+        if (time == 0) break;
+
+        count++;
+        if (count == 1 || time < bestTime) {
+            bestTime = time;
+        }
+
+        cout << "Текущий лучший результат: " << bestTime << " сек.\n";
+    }
+
+    if (count > 0) {
+        cout << "Финальный лучший результат: " << bestTime << " сек.\n";
+    }
+
+    return 0;
+}
