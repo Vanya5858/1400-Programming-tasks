30a663b712efb858ea72c0db8a1a9ec5f65e3a69
Ivan <iv.vano77@mail.ru>
2025-12-19 23:23:01 +0300

    Add files via upload

---
+++ Сhapter_7/Task_138.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int days;
+    cout << "Введите количество дней в месяце: ";
+    cin >> days;
+
+    double maxTemp, temp;
+    cin >> maxTemp;
+
+    for (int i = 1; i < days; i++) {
+        cin >> temp;
+        if (temp > maxTemp) {
+            maxTemp = temp;
+        }
+    }
+
+    cout << "Максимальная температура месяца: " << maxTemp << " C\n";
+    return 0;
+}
