fece1cc58e1cc48b4b58b264aa97d4af2dd74b2c
Ivan <iv.vano77@mail.ru>
2025-12-19 23:21:41 +0300

    Add files via upload

---
+++ Сhapter_7/Task_134.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int num, prev, countDistinct = 0;
+    bool first = true;
+
+    cout << "Введите последовательность вещественных чисел, оканчивающуюся 0: ";
+    while (true) {
+        cin >> num;
+        if (num == 0) break;
+
+        if (first) {
+            countDistinct = 1;
+            prev = num;
+            first = false;
+        }
+        else {
+            if (num != prev) {
+                countDistinct++;
+                prev = num;
+            }
+        }
+    }
+
+    cout << "Количество различных чисел: " << countDistinct << endl;
+    return 0;
+}
