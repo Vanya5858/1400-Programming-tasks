157eb6cb6ec816c09b013615ac7deb0ccb5e06b2
Ivan <iv.vano77@mail.ru>
2025-12-19 23:22:41 +0300

    Add files via upload

---
+++ Сhapter_7/Task_137.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество городов: ";
+    cin >> n;
+
+    double maxDist = 0, dist;
+
+    cout << "Введите расстояния от Москвы до каждого города: ";
+    for (int i = 0; i < n; i++) {
+        cin >> dist;
+        if (dist > maxDist) {
+            maxDist = dist;
+        }
+    }
+
+    cout << "Расстояние до самого удаленного города: " << maxDist << " км\n";
+    return 0;
+}
