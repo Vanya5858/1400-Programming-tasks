77c193535021f6e3e6a3215087eab4534d8f4560
Ivan <iv.vano77@mail.ru>
2025-12-23 23:46:57 +0300

    Add files via upload

---
+++ Сhapter_7/Task_146.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество человек: ";
+    cin >> n;
+
+    double maxHeight = 0, minHeight = 1e9;
+
+    cout << "Введите рост каждого человека: ";
+    for (int i = 0; i < n; i++) {
+        double height;
+        cin >> height;
+
+        if (height > maxHeight) maxHeight = height;
+        if (height < minHeight) minHeight = height;
+    }
+
+    double difference = maxHeight - minHeight;
+    cout << "Разница между ростом самого высокого и самого низкого: "
+        << difference << " см\n";
+
+    return 0;
+}
