26028da9cb5d2d2b5fe629808152086a5f4af53c
Ivan <iv.vano77@mail.ru>
2025-12-23 23:47:51 +0300

    Add files via upload

---
+++ Сhapter_7/Task_150.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+
+    int maxLength = 0, currentLength = 0;
+
+    cout << "Введите " << n << " целых чисел: ";
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+
+        if (num % 2 == 0) {
+            currentLength++;
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+            }
+        }
+        else {
+            currentLength = 0;
+        }
+    }
+
+    cout << "Наибольшая длина отрезка из четных чисел: " << maxLength << endl;
+    return 0;
+}
