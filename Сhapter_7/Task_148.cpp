667bc4f8b7b0524dfad057764d6ed554ee73b177
Ivan <iv.vano77@mail.ru>
2025-12-23 23:47:26 +0300

    Add files via upload

---
+++ Сhapter_7/Task_148.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество чисел n: ";
+    cin >> n;
+
+    int maxNum, minNum, num;
+    cin >> num;
+    maxNum = minNum = num;
+
+    for (int i = 1; i < n; i++) {
+        cin >> num;
+        if (num > maxNum) maxNum = num;
+        if (num < minNum) minNum = num;
+    }
+
+    bool condition = (maxNum - minNum) <= 25;
+    cout << "Максимальное (" << maxNum << ") превышает минимальное ("
+        << minNum << ") не более чем на 25: "
+        << (condition ? "Верно" : "Неверно") << endl;
+
+    return 0;
+}
