bdd847e08577f26b849d4926bf1dd4ec443a4885
Ivan <iv.vano77@mail.ru>
2025-12-23 23:46:39 +0300

    Add files via upload

---
+++ Сhapter_7/Task_145.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество судей: ";
+    cin >> n;
+
+    vector<double> scores(n);
+    cout << "Введите оценки судей: ";
+    for (int i = 0; i < n; i++) {
+        cin >> scores[i];
+    }
+
+    // Находим максимальную и минимальную оценки
+    double maxScore = scores[0], minScore = scores[0];
+    int maxIndex = 0, minIndex = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (scores[i] > maxScore) {
+            maxScore = scores[i];
+            maxIndex = i;
+        }
+        if (scores[i] < minScore) {
+            minScore = scores[i];
+            minIndex = i;
+        }
+    }
+
+    // Удаляем по одному максимуму и минимуму
+    double sum = 0;
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (i == maxIndex || i == minIndex) continue;
+        sum += scores[i];
+        count++;
+    }
+
+    if (count > 0) {
+        cout << "Оценка спортсмена: " << sum / count << endl;
+    }
+    else {
+        cout << "Недостаточно данных\n";
+    }
+
+    return 0;
+}
