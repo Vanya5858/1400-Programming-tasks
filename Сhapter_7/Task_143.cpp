9d83330b325f94e3860edcbd460ac3a584e1c1ae
Ivan <iv.vano77@mail.ru>
2025-12-23 23:46:11 +0300

    Add files via upload

---
+++ Сhapter_7/Task_143.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const int N = 16;
+    double minDensity = 1e9;
+
+    cout << "Введите численность (млн) и площадь (тыс. км^2) 16 государств:\n";
+    for (int i = 0; i < N; i++) {
+        double population, area;
+        cin >> population >> area;
+
+        double density = population / area; // млн/тыс.км² = тыс.чел/км²
+        if (density < minDensity) {
+            minDensity = density;
+        }
+    }
+
+    cout << "Минимальная плотность населения: " << minDensity << " тыс.чел/км^2";
+    return 0;
+}
