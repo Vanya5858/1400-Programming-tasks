db540ba5e44bc002afe6b4a927bdb43636e48b9f
Ivan <iv.vano77@mail.ru>
2025-12-23 23:46:26 +0300

    Add files via upload

---
+++ Сhapter_7/Task_144.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    int n;
+    cout << "Введите количество пар n: ";
+    cin >> n;
+
+    double maxSum = -1e9, minProd = 1e9;
+
+    cout << "Введите " << n << " пар чисел (a b):\n";
+    for (int i = 0; i < n; i++) {
+        double a, b;
+        cin >> a >> b;
+
+        double sum = a + b;
+        double prod = a * b;
+
+        if (sum > maxSum) maxSum = sum;
+        if (prod < minProd) minProd = prod;
+    }
+
+    cout << "Максимальная сумма: " << maxSum << endl;
+    cout << "Минимальное произведение: " << minProd << endl;
+    return 0;
+}
