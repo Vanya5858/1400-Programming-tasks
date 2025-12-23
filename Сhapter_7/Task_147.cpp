2f3c40eddeab146e967fcf92081e997c23cd4503
Ivan <iv.vano77@mail.ru>
2025-12-23 23:47:09 +0300

    Add files via upload

---
+++ Сhapter_7/Task_147.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    const int N = 20;
+    int maxStudents = 0, minStudents = 1000;
+
+    cout << "Введите количество учеников в 20 классах: ";
+    for (int i = 0; i < N; i++) {
+        int students;
+        cin >> students;
+
+        if (students > maxStudents) maxStudents = students;
+        if (students < minStudents) minStudents = students;
+    }
+
+    int difference = maxStudents - minStudents;
+    cout << "Разница между самым большим и самым маленьким классом: "
+        << difference << " учеников\n";
+
+    return 0;
+}
